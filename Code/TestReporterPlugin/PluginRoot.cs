6815699cb0a4000f0748777f54bf4a1315b7f3d3
wcss <wcss2020@gmail.com>
2019-08-15 09:55:24 +0800

    ...

--- Code/TestReporterPlugin/PluginRoot.cs
+++ Code/TestReporterPlugin/PluginRoot.cs
@@ -8,8 +8,6 @@ namespace TestReporterPlugin
 {
     public class PluginRoot:IReportPluginRoot
     {
-        private ImageList TreeViewImageList;
-
         /// <summary>
         /// 程序标题
         /// </summary>
@@ -55,57 +53,132 @@ namespace TestReporterPlugin
         /// <param name="defaultHintLabel">默认的提示标签</param>
         public override void init(Form mainForm, ToolStrip topToolStrip, ImageList treeViewImageListObj, TreeView treeViewObj, Panel contentObj, StatusStrip bottomStatusStrip, ToolStripStatusLabel defaultHintLabel)
         {
+            //添加点击事件
             treeViewObj.AfterSelect += treeViewObj_AfterSelect;
 
+            #region 构建树结构
             TreeNode firstNode = new TreeNode();
             firstNode.Name = "root";
             firstNode.Text = "申报书";
 
-            TreeNode item01 = new TreeNode();
-            firstNode.Nodes.Add(item01);
+            TreeNode itemObj = new TreeNode();
+            itemObj.Text = "概述";
+            firstNode.Nodes.Add(itemObj);
 
-            TreeNode item02 = new TreeNode();
-            firstNode.Nodes.Add(item02);
+            TreeNode subItemObj = new TreeNode();
+            subItemObj.Text = "项目摘要";
+            itemObj.Nodes.Add(subItemObj);
 
-            TreeNode item03 = new TreeNode();
-            firstNode.Nodes.Add(item03);
+            subItemObj = new TreeNode();
+            subItemObj.Text = "基本概念及内涵";
+            itemObj.Nodes.Add(subItemObj);
 
-            TreeNode item04 = new TreeNode();
-            firstNode.Nodes.Add(item04);
+            subItemObj = new TreeNode();
+            subItemObj.Text = "军事需求分析";
+            itemObj.Nodes.Add(subItemObj);
 
-            TreeNode item05 = new TreeNode();
-            firstNode.Nodes.Add(item05);
+            subItemObj = new TreeNode();
+            subItemObj.Text = "研究现状";
+            itemObj.Nodes.Add(subItemObj);
 
-            TreeNode item06 = new TreeNode();
-            firstNode.Nodes.Add(item06);
+            itemObj = new TreeNode();
+            itemObj.Text = "研究目标";
+            firstNode.Nodes.Add(itemObj);
 
-            TreeNode item07 = new TreeNode();
-            firstNode.Nodes.Add(item07);
+            itemObj = new TreeNode();
+            itemObj.Text = "基础性问题";
+            firstNode.Nodes.Add(itemObj);
 
-            TreeNode item08 = new TreeNode();
-            firstNode.Nodes.Add(item08);
+            itemObj = new TreeNode();
+            itemObj.Text = "项目分解";
+            firstNode.Nodes.Add(itemObj);
 
-            TreeNode item09 = new TreeNode();
-            firstNode.Nodes.Add(item09);
+            subItemObj = new TreeNode();
+            subItemObj.Text = "课题列表";
+            itemObj.Nodes.Add(subItemObj);
 
-            TreeNode item10 = new TreeNode();
-            firstNode.Nodes.Add(item10);
+            subItemObj = new TreeNode();
+            subItemObj.Text = "课题之间的关系";
+            itemObj.Nodes.Add(subItemObj);
 
-            TreeNode item11 = new TreeNode();
-            firstNode.Nodes.Add(item11);
+            subItemObj = new TreeNode();
+            subItemObj.Text = "课题详细";
+            itemObj.Nodes.Add(subItemObj);
 
-            TreeNode item12 = new TreeNode();
-            firstNode.Nodes.Add(item12);
+            itemObj = new TreeNode();
+            itemObj.Text = "研究成果";
+            firstNode.Nodes.Add(itemObj);
 
-            TreeNode item13 = new TreeNode();
-            firstNode.Nodes.Add(item13);
+            subItemObj = new TreeNode();
+            subItemObj.Text = "研究成果及考核指标";
+            itemObj.Nodes.Add(subItemObj);
 
-            TreeNode item14 = new TreeNode();
-            firstNode.Nodes.Add(item14);
+            subItemObj = new TreeNode();
+            subItemObj.Text = "评估方案";
+            itemObj.Nodes.Add(subItemObj);
+
+            subItemObj = new TreeNode();
+            subItemObj.Text = "预期效益";
+            itemObj.Nodes.Add(subItemObj);
+
+            itemObj = new TreeNode();
+            itemObj.Text = "研究周期、阶段划分和经费安排";
+            firstNode.Nodes.Add(itemObj);
+
+            subItemObj = new TreeNode();
+            subItemObj.Text = "项目阶段划分和经费安排";
+            itemObj.Nodes.Add(subItemObj);
+
+            subItemObj = new TreeNode();
+            subItemObj.Text = "课题阶段划分和经费安排";
+            itemObj.Nodes.Add(subItemObj);
+
+            itemObj = new TreeNode();
+            itemObj.Text = "项目负责人和研究团队";
+            firstNode.Nodes.Add(itemObj);
+
+            subItemObj = new TreeNode();
+            subItemObj.Text = "项目负责人";
+            itemObj.Nodes.Add(subItemObj);
+
+            subItemObj = new TreeNode();
+            subItemObj.Text = "研究团队";
+            itemObj.Nodes.Add(subItemObj);
+
+            subItemObj = new TreeNode();
+            subItemObj.Text = "各课题负责人及研究骨干情况表";
+            itemObj.Nodes.Add(subItemObj);
+
+            itemObj = new TreeNode();
+            itemObj.Text = "研究基础与保障条件";
+            firstNode.Nodes.Add(itemObj);
+
+            itemObj = new TreeNode();
+            itemObj.Text = "组织实施与风险控制";
+            firstNode.Nodes.Add(itemObj);
+
+            itemObj = new TreeNode();
+            itemObj.Text = "与有关计划关系";
+            firstNode.Nodes.Add(itemObj);
+
+            itemObj = new TreeNode();
+            itemObj.Text = "经费预算表";
+            firstNode.Nodes.Add(itemObj);
+
+            itemObj = new TreeNode();
+            itemObj.Text = "附件1-经费概算";
+            firstNode.Nodes.Add(itemObj);
+
+            itemObj = new TreeNode();
+            itemObj.Text = "附件2-保密资质";
+            firstNode.Nodes.Add(itemObj);
 
             treeViewObj.Nodes.Add(firstNode);
 
-            defaultHintLabel.Text = "大大大大大大大大大大大大大大";
+            firstNode.Expand();
+            #endregion
+
+            defaultHintLabel.Text = "大大大大大大大大大大大大大大," + WorkDir;
         }
 
         void treeViewObj_AfterSelect(object sender, TreeViewEventArgs e)
