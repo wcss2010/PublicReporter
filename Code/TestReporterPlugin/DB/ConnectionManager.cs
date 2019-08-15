cb1bf4b00dedabe6695bb46058d7df0a2852228a
wcss <wcss2020@gmail.com>
2019-08-15 16:27:32 +0800

    ...

---
+++ Code/TestReporterPlugin/DB/ConnectionManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Noear.Weed;
+
+namespace TestReporterPlugin.DB
+{
+    public class ConnectionManager
+    {
+        private static System.Data.SQLite.SQLiteFactory factory = null;
+
+        /// <summary>
+        /// 数据库上下文
+        /// </summary>
+        public static DbContext Context { get; set; }
+
+        /// <summary>
+        /// 初始化数据库连接
+        /// </summary>
+        /// <param name="dbFile"></param>
+        public static void Open(string dbFile)
+        {
+            factory = new System.Data.SQLite.SQLiteFactory();
+            Context = new DbContext("main", "Data Source=" + dbFile, factory);
+            Context.IsSupportInsertAfterSelectIdentity = false;
+            Context.IsSupportGCAfterDispose = true;
+        }
+
+        public static void Close()
+        {
+            try
+            {
+                factory.Dispose();
+            }
+            catch (Exception ex) { }
+            factory = null;
+
+            try
+            {
+                Context.Dispose();
+            }
+            catch (Exception ex) { }
+            Context = null;
+        }
+    }
+}
---
+++ Code/TestReporterPlugin/DB/Entitys/MoneyAndType.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data;
+using System.Text;
+
+namespace TestReporterPlugin.DB.Entitys
+{
+    /// <summary>
+    /// 类MoneyAndType。
+    /// </summary>
+    [Serializable]
+    public partial class MoneyAndType : IEntity
+    {
+        public MoneyAndType() { }
+
+        public override Noear.Weed.DbTableQuery copyTo(Noear.Weed.DbTableQuery query)
+        {
+            //设置值
+            query.set("ID", ID);
+            query.set("ProjectID", ProjectID);
+            query.set("ItemData", ItemData);
+
+            return query;
+        }
+
+        #region Model
+        private string _id;
+        private string _projectid;
+        private string _itemdata;
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ID
+        {
+            set { _id = value; }
+            get { return _id; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ProjectID
+        {
+            set { _projectid = value; }
+            get { return _projectid; }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ItemData
+        {
+            set { _itemdata = value; }
+            get { return _itemdata; }
+        }
+        #endregion Model
+
+        public override void bind(Noear.Weed.GetHandlerEx source)
+        {
+            ID = source("ID").value<string>(Guid.NewGuid().ToString());
+            ProjectID = source("ProjectID").value<string>(string.Empty);
+            ItemData = source("ItemData").value<string>(string.Empty);
+        }
+
+        public override Noear.Weed.IBinder clone()
+        {
+            return new MoneyAndType();
+        }
+    }
+}
