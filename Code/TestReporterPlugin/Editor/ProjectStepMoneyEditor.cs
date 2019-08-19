eca4c3fc18421c5d69782a815a858ce172d1d3cc
wcss <wcss2020@gmail.com>
2019-08-19 15:49:49 +0800

    ...

---
+++ Code/TestReporterPlugin/Editor/ProjectStepMoneyEditor.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TestReporterPlugin.DB;
+using TestReporterPlugin.DB.Entitys;
+
+namespace TestReporterPlugin.Editor
+{
+    public partial class ProjectStepMoneyEditor : BaseEditor
+    {
+        public ProjectStepMoneyEditor()
+        {
+            InitializeComponent();
+
+            //dgvDetail[dgvDetail.Columns.Count - 1, 0].Value = global::ProjectReporter.Properties.Resources.DELETE_28;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            ProjectReporter.Forms.UIDoWorkProcessForm upf = new Forms.UIDoWorkProcessForm();
+            upf.EnabledDisplayProgress = false;
+            upf.LabalText = "正在保存,请等待...";
+            upf.ShowProgress();
+
+            try
+            {
+                OnSaveEvent();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("保存失败！Ex:" + ex.ToString());
+            }
+            finally
+            {
+                upf.Close();
+            }
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            OnLastEvent();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            OnNextEvent();
+        }
+
+        private void dgvDetail_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+            ((KryptonDataGridView)sender)[((KryptonDataGridView)sender).Columns.Count - 1, e.RowIndex == 0 ? e.RowIndex : e.RowIndex - 1].Value = global::ProjectReporter.Properties.Resources.DELETE_28;
+        }
+
+        private void dgvDetail_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        
+        public override void ClearView()
+        {
+            base.ClearView();
+
+            dgvDetail.Rows.Clear();
+        }
+
+        public override void RefreshView()
+        {
+            base.RefreshView();
+
+            UpdateStepList();
+        }
+
+        private void UpdateStepList()
+        {
+            StepList = ConnectionManager.Context.table("Step").where("ProjectID='" + MainForm.Instance.ProjectObj.ID + "'").select("*").getList<Step>(new Step());
+            if (StepList != null)
+            {
+                int indexx = 0;
+                dgvDetail.Rows.Clear();
+                foreach (Step step in StepList)
+                {
+                    string stepContent = string.Empty;
+                    ProjectAndStep projectAndStep = ConnectionManager.Context.table("ProjectAndStep").where("StepID='" + step.ID + "'").select("*").getItem<ProjectAndStep>(new ProjectAndStep());
+                    if (projectAndStep != null)
+                    {
+                        stepContent = projectAndStep.StepContent;
+                    }
+
+                    indexx++;
+                    List<object> cells = new List<object>();
+                    cells.Add(indexx + "");
+                    cells.Add(step.StepIndex);
+                    cells.Add(stepContent);
+                    cells.Add(step.StepDest);
+                    cells.Add(step.StepMoney);
+
+                    int rowIndex = dgvDetail.Rows.Add(cells.ToArray());
+                    dgvDetail.Rows[rowIndex].Tag = step;
+                }  
+            }
+        }
+
+        public override void OnSaveEvent()
+        {
+            base.OnSaveEvent();
+
+            foreach (DataGridViewRow dgvRow in dgvDetail.Rows)
+            {
+                if (dgvRow.Tag != null)
+                {
+                    Step step = (Step)dgvRow.Tag;
+
+                    if (dgvRow.Cells[2].Value == null || string.IsNullOrEmpty(dgvRow.Cells[2].Value.ToString()))
+                    {
+                        MessageBox.Show("对不起,请输入阶段内容");
+                        return;
+                    }
+                    if (dgvRow.Cells[3].Value == null || string.IsNullOrEmpty(dgvRow.Cells[3].Value.ToString()))
+                    {
+                        MessageBox.Show("对不起,请输入阶段目标");
+                        return;
+                    }
+                    if (dgvRow.Cells[4].Value == null || string.IsNullOrEmpty(dgvRow.Cells[4].Value.ToString()))
+                    {
+                        MessageBox.Show("对不起,请输入阶段经费");
+                        return;
+                    }
+
+                    ProjectAndStep pas = ConnectionManager.Context.table("ProjectAndStep").where("StepID='" + step.ID + "'").select("*").getItem<ProjectAndStep>(new ProjectAndStep());
+                    if (pas != null)
+                    {
+                        pas.StepContent = dgvRow.Cells[2].Value.ToString();
+                        pas.copyTo(ConnectionManager.Context.table("ProjectAndStep")).where("ID='" + pas.ID + "'").update();
+                    }
+
+                    step.StepDest = dgvRow.Cells[3].Value.ToString();
+                    step.StepMoney = decimal.Parse(dgvRow.Cells[4].Value.ToString());
+
+                    step.copyTo(ConnectionManager.Context.table("Step")).where("ID='" + step.ID + "'").update();
+                }
+            }
+
+            UpdateStepList();
+        }
+
+        public List<Step> StepList { get; set; }
+
+        public override bool IsInputCompleted()
+        {
+            if (dgvDetail.Rows.Count == 0)
+            {
+                MessageBox.Show("对不起,请输入项目阶段信息!");
+            }
+
+            return dgvDetail.Rows.Count >= 1;
+        }
+    }
+}
---
+++ Code/TestReporterPlugin/Editor/ProjectStepMoneyEditor.designer.cs
@@ -0,0 +1,271 @@
+﻿namespace TestReporterPlugin.Editor
+{
+    partial class ProjectStepMoneyEditor
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.tableLayoutPanel15 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.plTitle = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.plContent = new System.Windows.Forms.Panel();
+            this.dgvDetail = new System.Windows.Forms.DataGridView();
+            this.selpersonid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colJieDuanHuaFen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colYanJiuNeiRong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colJieDuanMuBiao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colJieDuanJingFei = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.delete = new System.Windows.Forms.DataGridViewImageColumn();
+            this.tableLayoutPanel15.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.plTitle)).BeginInit();
+            this.plTitle.SuspendLayout();
+            this.plContent.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel15
+            // 
+            this.tableLayoutPanel15.ColumnCount = 3;
+            this.tableLayoutPanel15.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel15.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel15.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel15.Controls.Add(this.tableLayoutPanel1, 1, 3);
+            this.tableLayoutPanel15.Controls.Add(this.plTitle, 1, 1);
+            this.tableLayoutPanel15.Controls.Add(this.plContent, 1, 2);
+            this.tableLayoutPanel15.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel15.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel15.Name = "tableLayoutPanel15";
+            this.tableLayoutPanel15.RowCount = 5;
+            this.tableLayoutPanel15.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel15.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel15.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel15.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel15.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel15.Size = new System.Drawing.Size(1043, 637);
+            this.tableLayoutPanel15.TabIndex = 4;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 0F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btnLast, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnSave, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnNext, 3, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(53, 580);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(937, 34);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(840, 3);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(1, 25);
+            this.btnLast.TabIndex = 1;
+            this.btnLast.Text = "返回";
+            this.btnLast.Visible = false;
+            this.btnLast.Click += new System.EventHandler(this.btnLast_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(740, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(90, 27);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "保存";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(840, 3);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(90, 27);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "下一步";
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // plTitle
+            // 
+            this.plTitle.Controls.Add(this.label1);
+            this.plTitle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plTitle.Location = new System.Drawing.Point(54, 20);
+            this.plTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.plTitle.Name = "plTitle";
+            this.plTitle.Padding = new System.Windows.Forms.Padding(6, 0, 0, 0);
+            this.plTitle.Size = new System.Drawing.Size(935, 30);
+            this.plTitle.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F);
+            this.label1.Location = new System.Drawing.Point(6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(929, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "项目阶段划分和经费安排";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // plContent
+            // 
+            this.plContent.BackColor = System.Drawing.Color.Transparent;
+            this.plContent.Controls.Add(this.dgvDetail);
+            this.plContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plContent.Location = new System.Drawing.Point(53, 53);
+            this.plContent.Name = "plContent";
+            this.plContent.Size = new System.Drawing.Size(937, 521);
+            this.plContent.TabIndex = 7;
+            // 
+            // dgvDetail
+            // 
+            this.dgvDetail.AllowUserToAddRows = false;
+            this.dgvDetail.AllowUserToResizeRows = false;
+            this.dgvDetail.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            this.dgvDetail.ColumnHeadersHeight = 35;
+            this.dgvDetail.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.selpersonid,
+            this.colJieDuanHuaFen,
+            this.colYanJiuNeiRong,
+            this.colJieDuanMuBiao,
+            this.colJieDuanJingFei,
+            this.delete});
+            this.dgvDetail.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDetail.Location = new System.Drawing.Point(0, 0);
+            this.dgvDetail.MultiSelect = false;
+            this.dgvDetail.Name = "dgvDetail";
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvDetail.RowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvDetail.RowTemplate.Height = 35;
+            this.dgvDetail.Size = new System.Drawing.Size(937, 521);
+            this.dgvDetail.StateCommon.Background.Color1 = System.Drawing.Color.White;
+            this.dgvDetail.StateCommon.BackStyle = ComponentFactory.Krypton.Toolkit.PaletteBackStyle.GridBackgroundList;
+            this.dgvDetail.StateCommon.HeaderColumn.Content.Font = new System.Drawing.Font("黑体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dgvDetail.StateCommon.HeaderColumn.Content.TextH = ComponentFactory.Krypton.Toolkit.PaletteRelativeAlign.Center;
+            this.dgvDetail.TabIndex = 5;
+            this.dgvDetail.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDetail_CellContentClick);
+            this.dgvDetail.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dgvDetail_RowsAdded);
+            // 
+            // selpersonid
+            // 
+            this.selpersonid.HeaderText = "序号";
+            this.selpersonid.Name = "selpersonid";
+            this.selpersonid.ReadOnly = true;
+            this.selpersonid.Width = 60;
+            // 
+            // colJieDuanHuaFen
+            // 
+            this.colJieDuanHuaFen.HeaderText = "阶段";
+            this.colJieDuanHuaFen.Name = "colJieDuanHuaFen";
+            this.colJieDuanHuaFen.ReadOnly = true;
+            this.colJieDuanHuaFen.Width = 60;
+            // 
+            // colYanJiuNeiRong
+            // 
+            this.colYanJiuNeiRong.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colYanJiuNeiRong.HeaderText = "研究内容";
+            this.colYanJiuNeiRong.Name = "colYanJiuNeiRong";
+            // 
+            // colJieDuanMuBiao
+            // 
+            this.colJieDuanMuBiao.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colJieDuanMuBiao.HeaderText = "阶段目标";
+            this.colJieDuanMuBiao.Name = "colJieDuanMuBiao";
+            // 
+            // colJieDuanJingFei
+            // 
+            this.colJieDuanJingFei.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colJieDuanJingFei.HeaderText = "阶段经费";
+            this.colJieDuanJingFei.Maximum = new decimal(new int[] {
+            1410065407,
+            2,
+            0,
+            0});
+            this.colJieDuanJingFei.Name = "colJieDuanJingFei";
+            this.colJieDuanJingFei.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.colJieDuanJingFei.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.colJieDuanJingFei.Width = 259;
+            // 
+            // delete
+            // 
+            this.delete.HeaderText = "删除";
+            this.delete.Image = global::ProjectReporter.Properties.Resources.DELETE_16;
+            this.delete.Name = "delete";
+            this.delete.ReadOnly = true;
+            this.delete.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.delete.Visible = false;
+            this.delete.Width = 60;
+            // 
+            // XiangMuJieDuanHuaFenEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel15);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "XiangMuJieDuanHuaFenEditor";
+            this.Size = new System.Drawing.Size(1043, 637);
+            this.tableLayoutPanel15.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.plTitle)).EndInit();
+            this.plTitle.ResumeLayout(false);
+            this.plContent.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel15;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Panel plTitle;
+        private System.Windows.Forms.Panel plContent;
+        private System.Windows.Forms.DataGridView dgvDetail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn selpersonid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colJieDuanHuaFen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colYanJiuNeiRong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colJieDuanMuBiao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colJieDuanJingFei;
+        private System.Windows.Forms.DataGridViewImageColumn delete;
+    }
+}
