5f6d5c98978e34d74fa239f104d4967bc5d907c2
wcss <wcss2020@gmail.com>
2019-08-22 17:21:05 +0800

    ...

--- Code/TestReporterPlugin/Editor/SubjectStepMoneyEditor.designer.cs
+++ Code/TestReporterPlugin/Editor/SubjectStepMoneyEditor.designer.cs
@@ -32,21 +32,26 @@
             this.tableLayoutPanel15 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.btnSave = new System.Windows.Forms.Button();
+            this.btnExcelLoad = new System.Windows.Forms.Button();
+            this.lklDownloadFuJian = new System.Windows.Forms.LinkLabel();
             this.plTitle = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.plContent = new System.Windows.Forms.Panel();
             this.dgvDetail = new System.Windows.Forms.DataGridView();
             this.selpersonid = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colJieDuanHuaFen = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colYanJiuNeiRong = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colJieDuanMuBiao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colKeTi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colJieDuanHuaHuaFen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDest = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colContent = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colResult = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTarget = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colJieDuanJingFei = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.delete = new System.Windows.Forms.DataGridViewImageColumn();
+            this.ofdExcelDialog = new System.Windows.Forms.OpenFileDialog();
             this.tableLayoutPanel15.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.plTitle.SuspendLayout();
             this.plContent.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel15
@@ -67,33 +72,60 @@
             this.tableLayoutPanel15.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel15.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             this.tableLayoutPanel15.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel15.Size = new System.Drawing.Size(1043, 637);
-            this.tableLayoutPanel15.TabIndex = 4;
+            this.tableLayoutPanel15.Size = new System.Drawing.Size(921, 623);
+            this.tableLayoutPanel15.TabIndex = 5;
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnCount = 6;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 130F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 0F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.btnSave, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnSave, 3, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnExcelLoad, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lklDownloadFuJian, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(53, 580);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(53, 566);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(937, 34);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(815, 34);
             this.tableLayoutPanel1.TabIndex = 6;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(740, 3);
+            this.btnSave.Location = new System.Drawing.Point(618, 3);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(90, 27);
             this.btnSave.TabIndex = 0;
             this.btnSave.Text = "保存";
             this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnExcelLoad
+            // 
+            this.btnExcelLoad.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnExcelLoad.Location = new System.Drawing.Point(518, 3);
+            this.btnExcelLoad.Name = "btnExcelLoad";
+            this.btnExcelLoad.Size = new System.Drawing.Size(94, 28);
+            this.btnExcelLoad.TabIndex = 4;
+            this.btnExcelLoad.Text = "从Excel导入";
+            this.btnExcelLoad.Click += new System.EventHandler(this.btnExcelLoad_Click);
+            // 
+            // lklDownloadFuJian
+            // 
+            this.lklDownloadFuJian.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lklDownloadFuJian.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lklDownloadFuJian.Location = new System.Drawing.Point(202, 0);
+            this.lklDownloadFuJian.Name = "lklDownloadFuJian";
+            this.lklDownloadFuJian.Size = new System.Drawing.Size(180, 34);
+            this.lklDownloadFuJian.TabIndex = 5;
+            this.lklDownloadFuJian.TabStop = true;
+            this.lklDownloadFuJian.Text = "课题阶段划分导入模板.xls";
+            this.lklDownloadFuJian.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lklDownloadFuJian.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lklDownloadFuJian_LinkClicked);
             // 
             // plTitle
             // 
@@ -103,19 +135,19 @@
             this.plTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.plTitle.Name = "plTitle";
             this.plTitle.Padding = new System.Windows.Forms.Padding(6, 0, 0, 0);
-            this.plTitle.Size = new System.Drawing.Size(935, 30);
+            this.plTitle.Size = new System.Drawing.Size(813, 30);
             this.plTitle.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Font = new System.Drawing.Font("仿宋", 14.25F);
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F);
             this.label1.Location = new System.Drawing.Point(6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(929, 30);
+            this.label1.Size = new System.Drawing.Size(807, 30);
             this.label1.TabIndex = 1;
-            this.label1.Text = "项目阶段划分和经费安排";
+            this.label1.Text = "课题阶段划分和经费安排";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // plContent
@@ -125,92 +157,130 @@
             this.plContent.Dock = System.Windows.Forms.DockStyle.Fill;
             this.plContent.Location = new System.Drawing.Point(53, 53);
             this.plContent.Name = "plContent";
-            this.plContent.Size = new System.Drawing.Size(937, 521);
+            this.plContent.Size = new System.Drawing.Size(815, 507);
             this.plContent.TabIndex = 7;
             // 
             // dgvDetail
             // 
             this.dgvDetail.AllowUserToAddRows = false;
             this.dgvDetail.AllowUserToResizeRows = false;
-            this.dgvDetail.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            this.dgvDetail.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCellsExceptHeader;
+            this.dgvDetail.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
             this.dgvDetail.ColumnHeadersHeight = 35;
             this.dgvDetail.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.selpersonid,
-            this.colJieDuanHuaFen,
-            this.colYanJiuNeiRong,
-            this.colJieDuanMuBiao,
+            this.colKeTi,
+            this.colJieDuanHuaHuaFen,
+            this.colDest,
+            this.colContent,
+            this.colResult,
+            this.colTarget,
             this.colJieDuanJingFei,
             this.delete});
             this.dgvDetail.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgvDetail.Location = new System.Drawing.Point(0, 0);
             this.dgvDetail.MultiSelect = false;
             this.dgvDetail.Name = "dgvDetail";
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("仿宋", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.dgvDetail.RowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dgvDetail.RowTemplate.Height = 35;
-            this.dgvDetail.Size = new System.Drawing.Size(937, 521);
+            this.dgvDetail.Size = new System.Drawing.Size(815, 507);
             this.dgvDetail.TabIndex = 5;
             this.dgvDetail.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDetail_CellContentClick);
+            this.dgvDetail.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDetail_CellContentDoubleClick);
+            this.dgvDetail.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvDetail_CellMouseDoubleClick);
             this.dgvDetail.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dgvDetail_RowsAdded);
             // 
             // selpersonid
             // 
             this.selpersonid.HeaderText = "序号";
+            this.selpersonid.MinimumWidth = 80;
             this.selpersonid.Name = "selpersonid";
-            this.selpersonid.ReadOnly = true;
-            this.selpersonid.Width = 60;
+            this.selpersonid.Visible = false;
+            this.selpersonid.Width = 80;
             // 
-            // colJieDuanHuaFen
+            // colKeTi
             // 
-            this.colJieDuanHuaFen.HeaderText = "阶段";
-            this.colJieDuanHuaFen.Name = "colJieDuanHuaFen";
-            this.colJieDuanHuaFen.ReadOnly = true;
-            this.colJieDuanHuaFen.Width = 60;
+            this.colKeTi.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.colKeTi.HeaderText = "课题";
+            this.colKeTi.MinimumWidth = 69;
+            this.colKeTi.Name = "colKeTi";
+            this.colKeTi.ReadOnly = true;
+            this.colKeTi.Width = 69;
             // 
-            // colYanJiuNeiRong
+            // colJieDuanHuaHuaFen
             // 
-            this.colYanJiuNeiRong.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.colYanJiuNeiRong.HeaderText = "研究内容";
-            this.colYanJiuNeiRong.Name = "colYanJiuNeiRong";
+            this.colJieDuanHuaHuaFen.HeaderText = "阶段";
+            this.colJieDuanHuaHuaFen.MinimumWidth = 60;
+            this.colJieDuanHuaHuaFen.Name = "colJieDuanHuaHuaFen";
+            this.colJieDuanHuaHuaFen.ReadOnly = true;
+            this.colJieDuanHuaHuaFen.Width = 60;
             // 
-            // colJieDuanMuBiao
+            // colDest
             // 
-            this.colJieDuanMuBiao.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.colJieDuanMuBiao.HeaderText = "阶段目标";
-            this.colJieDuanMuBiao.Name = "colJieDuanMuBiao";
+            this.colDest.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colDest.HeaderText = "完成内容及阶段目标";
+            this.colDest.Name = "colDest";
+            this.colDest.ReadOnly = true;
+            // 
+            // colContent
+            // 
+            this.colContent.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colContent.HeaderText = "完成内容";
+            this.colContent.Name = "colContent";
+            this.colContent.Visible = false;
+            // 
+            // colResult
+            // 
+            this.colResult.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colResult.HeaderText = "阶段成果、考核指标及考核方式";
+            this.colResult.Name = "colResult";
+            this.colResult.ReadOnly = true;
+            // 
+            // colTarget
+            // 
+            this.colTarget.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colTarget.HeaderText = "考核指标";
+            this.colTarget.Name = "colTarget";
+            this.colTarget.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.colTarget.Visible = false;
             // 
             // colJieDuanJingFei
             // 
-            this.colJieDuanJingFei.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
-            this.colJieDuanJingFei.HeaderText = "阶段经费";
+            this.colJieDuanJingFei.HeaderText = "阶段经费(万)";
+            this.colJieDuanJingFei.MinimumWidth = 110;
             this.colJieDuanJingFei.Name = "colJieDuanJingFei";
             this.colJieDuanJingFei.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.colJieDuanJingFei.Width = 110;
             // 
             // delete
             // 
             this.delete.HeaderText = "删除";
             this.delete.Image = global::TestReporterPlugin.Resource.DELETE_16;
+            this.delete.MinimumWidth = 60;
             this.delete.Name = "delete";
             this.delete.ReadOnly = true;
             this.delete.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             this.delete.Visible = false;
             this.delete.Width = 60;
             // 
-            // SubjectStepMoneyEditor
+            // ofdExcelDialog
+            // 
+            this.ofdExcelDialog.Filter = "*.xls|*.xls";
+            // 
+            // KeTiJieDuanHuaFenEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Control;
             this.Controls.Add(this.tableLayoutPanel15);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "SubjectStepMoneyEditor";
-            this.Size = new System.Drawing.Size(1043, 637);
+            this.Size = new System.Drawing.Size(921, 623);
             this.tableLayoutPanel15.ResumeLayout(false);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.plTitle.ResumeLayout(false);
             this.plContent.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -221,13 +291,19 @@
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Panel plTitle;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel plContent;
         private System.Windows.Forms.DataGridView dgvDetail;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnExcelLoad;
+        private System.Windows.Forms.LinkLabel lklDownloadFuJian;
+        private System.Windows.Forms.OpenFileDialog ofdExcelDialog;
         private System.Windows.Forms.DataGridViewTextBoxColumn selpersonid;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colJieDuanHuaFen;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colYanJiuNeiRong;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colJieDuanMuBiao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colKeTi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colJieDuanHuaHuaFen;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDest;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colContent;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colResult;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTarget;
         private System.Windows.Forms.DataGridViewTextBoxColumn colJieDuanJingFei;
         private System.Windows.Forms.DataGridViewImageColumn delete;
     }
