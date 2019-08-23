89447c6479617a8ea825c24bbec1c3ff52abb28a
wcss <wcss2020@gmail.com>
2019-08-23 10:32:17 +0800

    ...

--- Code/TestReporterPlugin/Editor/DocumentPasteEditor.Designer.cs
+++ Code/TestReporterPlugin/Editor/DocumentPasteEditor.Designer.cs
@@ -31,10 +31,8 @@
             this.lblInfo = new TestReporterPlugin.Controls.AutoHeightLabel();
             this.plContent = new System.Windows.Forms.Panel();
             this.btnEditDocument = new System.Windows.Forms.Button();
-            this.tableLayoutPanel15 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
             this.plContent.SuspendLayout();
-            this.tableLayoutPanel15.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,9 +43,9 @@
             this.lblInfo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.lblInfo.Dock = System.Windows.Forms.DockStyle.Top;
             this.lblInfo.Font = new System.Drawing.Font("仿宋", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.lblInfo.Location = new System.Drawing.Point(0, 0);
+            this.lblInfo.Location = new System.Drawing.Point(10, 10);
             this.lblInfo.Name = "lblInfo";
-            this.lblInfo.Size = new System.Drawing.Size(1057, 46);
+            this.lblInfo.Size = new System.Drawing.Size(1063, 46);
             this.lblInfo.TabIndex = 0;
             this.lblInfo.Text = "label1";
             // 
@@ -56,9 +54,9 @@
             this.plContent.BackColor = System.Drawing.SystemColors.Control;
             this.plContent.Controls.Add(this.btnEditDocument);
             this.plContent.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.plContent.Location = new System.Drawing.Point(0, 46);
+            this.plContent.Location = new System.Drawing.Point(10, 56);
             this.plContent.Name = "plContent";
-            this.plContent.Size = new System.Drawing.Size(1057, 459);
+            this.plContent.Size = new System.Drawing.Size(1063, 505);
             this.plContent.TabIndex = 1;
             // 
             // btnEditDocument
@@ -73,33 +71,16 @@
             this.btnEditDocument.UseVisualStyleBackColor = false;
             this.btnEditDocument.Click += new System.EventHandler(this.btnEditDocument_Click);
             // 
-            // tableLayoutPanel15
-            // 
-            this.tableLayoutPanel15.ColumnCount = 3;
-            this.tableLayoutPanel15.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
-            this.tableLayoutPanel15.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel15.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 10F));
-            this.tableLayoutPanel15.Controls.Add(this.panel1, 1, 2);
-            this.tableLayoutPanel15.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel15.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel15.Name = "tableLayoutPanel15";
-            this.tableLayoutPanel15.RowCount = 5;
-            this.tableLayoutPanel15.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
-            this.tableLayoutPanel15.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel15.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel15.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel15.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 10F));
-            this.tableLayoutPanel15.Size = new System.Drawing.Size(1083, 571);
-            this.tableLayoutPanel15.TabIndex = 5;
-            // 
             // panel1
             // 
             this.panel1.Controls.Add(this.plContent);
             this.panel1.Controls.Add(this.lblInfo);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(13, 33);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1057, 505);
+            this.panel1.Padding = new System.Windows.Forms.Padding(10);
+            this.panel1.Size = new System.Drawing.Size(1083, 571);
             this.panel1.TabIndex = 0;
             // 
             // DocumentPasteEditor
@@ -107,11 +88,10 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
-            this.Controls.Add(this.tableLayoutPanel15);
+            this.Controls.Add(this.panel1);
             this.Name = "DocumentPasteEditor";
             this.Size = new System.Drawing.Size(1083, 571);
             this.plContent.ResumeLayout(false);
-            this.tableLayoutPanel15.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -122,7 +102,6 @@
         private TestReporterPlugin.Controls.AutoHeightLabel lblInfo;
         private System.Windows.Forms.Panel plContent;
         private System.Windows.Forms.Button btnEditDocument;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel15;
         private System.Windows.Forms.Panel panel1;
     }
 }
