6d6dbc9219beaf9fa119ef170a16f5783bf3d65e
wcss <wcss2020@gmail.com>
2019-08-26 10:54:52 +0800

    ...

--- Code/TestReporterPlugin/Utility/WordReView.cs
+++ Code/TestReporterPlugin/Utility/WordReView.cs
@@ -1,8 +1,10 @@
 ﻿using SuperCodeFactoryUILib.Forms;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace TestReporterPlugin.Utility
 {
@@ -22,23 +24,36 @@ namespace TestReporterPlugin.Utility
 
             Report(progressDialog, 10, "准备Word...", 1000);
 
-            Report(progressDialog, 20, "准备数据...", 1000);
+            string fileName = pt.projectObj.Name + "-项目建议书.docx";
+            WordUtility wu = new WordUtility();
+            wu.CreateNewDocument(Path.Combine(Path.Combine(pt.WorkDir, "Helper"), "newtemplete.docx"));
 
-            Report(progressDialog, 30, "写入基本信息...", 1000);
+            try
+            {
+                Report(progressDialog, 20, "准备数据...", 1000);
 
-            Report(progressDialog, 40, "写入文档文件...", 1000);
+                Report(progressDialog, 30, "写入基本信息...", 1000);
 
-            Report(progressDialog, 50, "写入阶段信息...", 1000);
+                Report(progressDialog, 40, "写入文档文件...", 1000);
 
-            Report(progressDialog, 60, "写入课题负责人及研究骨干信息...", 1000);
+                Report(progressDialog, 50, "写入阶段信息...", 1000);
 
-            Report(progressDialog, 70, "写入经费预算...", 1000);
+                Report(progressDialog, 60, "写入课题负责人及研究骨干信息...", 1000);
 
-            Report(progressDialog, 75, "写入联系方式...", 1000);
+                Report(progressDialog, 70, "写入经费预算...", 1000);
 
-            Report(progressDialog, 80, "更新目录...", 1000);
+                Report(progressDialog, 75, "写入联系方式...", 1000);
 
-            Report(progressDialog, 90, "生成文档...", 1000);
+                Report(progressDialog, 80, "更新目录...", 1000);
+
+                Report(progressDialog, 90, "生成文档...", 1000);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "提示", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+            Report(progressDialog, 95, "", 1000);
         }
 
         /// <summary>
