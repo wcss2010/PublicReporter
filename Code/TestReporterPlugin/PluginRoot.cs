6f6cfe50c89d5e4950daf78acf58d6a803bc3cd2
wcss <wcss2020@gmail.com>
2019-08-27 16:19:31 +0800

    ...

--- Code/TestReporterPlugin/PluginRoot.cs
+++ Code/TestReporterPlugin/PluginRoot.cs
@@ -536,6 +536,7 @@ namespace TestReporterPlugin
                     string errorPage = string.Empty;
                     if (!isInputCompleted(ref errorPage))
                     {
+                        MessageBox.Show("对不起，内容未填写完不能上报!", "提示", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                         MessageBox.Show("请将页签[" + errorPage + "]填写完整再点击上报!", "提示", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                         return;
                     }
