8e553b13475b76dc0fc5006670992b4219125224
wcss <wcss2020@gmail.com>
2019-09-06 11:40:23 +0800

    ...

--- Code/TestReporterPlugin/Editor/DocumentPasteEditor.Designer.cs
+++ Code/TestReporterPlugin/Editor/DocumentPasteEditor.Designer.cs
@@ -30,12 +30,15 @@
         {
             this.lblInfo = new TestReporterPlugin.Controls.AutoHeightLabel();
             this.plContent = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.wbWordView = new System.Windows.Forms.WebBrowser();
             this.panel2 = new System.Windows.Forms.Panel();
             this.lblWordInfo = new System.Windows.Forms.Label();
             this.btnEditDocument = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.plContent.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -56,7 +59,7 @@
             // plContent
             // 
             this.plContent.BackColor = System.Drawing.SystemColors.Control;
-            this.plContent.Controls.Add(this.wbWordView);
+            this.plContent.Controls.Add(this.panel3);
             this.plContent.Controls.Add(this.panel2);
             this.plContent.Dock = System.Windows.Forms.DockStyle.Fill;
             this.plContent.Location = new System.Drawing.Point(10, 56);
@@ -64,11 +67,33 @@
             this.plContent.Size = new System.Drawing.Size(1063, 505);
             this.plContent.TabIndex = 1;
             // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.wbWordView);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 38);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1063, 467);
+            this.panel3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("仿宋", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(4, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(557, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "这个是预览的文档，只用于查看，实际效果以Word中为准！";
+            // 
             // wbWordView
             // 
             this.wbWordView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.wbWordView.IsWebBrowserContextMenuEnabled = false;
-            this.wbWordView.Location = new System.Drawing.Point(0, 38);
+            this.wbWordView.Location = new System.Drawing.Point(0, 0);
             this.wbWordView.MinimumSize = new System.Drawing.Size(20, 20);
             this.wbWordView.Name = "wbWordView";
             this.wbWordView.Size = new System.Drawing.Size(1063, 467);
@@ -128,6 +153,8 @@
             this.Name = "DocumentPasteEditor";
             this.Size = new System.Drawing.Size(1083, 571);
             this.plContent.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -143,5 +170,7 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label lblWordInfo;
         private System.Windows.Forms.WebBrowser wbWordView;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
     }
 }
