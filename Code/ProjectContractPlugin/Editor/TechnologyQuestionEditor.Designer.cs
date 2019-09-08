feeca351ec9fcd5d859fdd98b49100cc0ae6fee1
wcss <wcss2020@gmail.com>
2019-09-09 06:36:10 +0800

    ...

--- Code/ProjectContractPlugin/Editor/TechnologyQuestionEditor.Designer.cs
+++ Code/ProjectContractPlugin/Editor/TechnologyQuestionEditor.Designer.cs
@@ -28,18 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblInfo = new ProjectContractPlugin.Controls.AutoHeightLabel();
+            this.plMain = new System.Windows.Forms.Panel();
+            this.plContent = new System.Windows.Forms.Panel();
+            this.dgvDetail = new System.Windows.Forms.DataGridView();
+            this.plButtons = new System.Windows.Forms.Panel();
+            this.btnDelAll = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.plMain.SuspendLayout();
+            this.plContent.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).BeginInit();
+            this.plButtons.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoHeight = true;
+            this.lblInfo.BackColor = System.Drawing.SystemColors.Control;
+            this.lblInfo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblInfo.Font = new System.Drawing.Font("仿宋", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblInfo.Location = new System.Drawing.Point(10, 10);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(1155, 46);
+            this.lblInfo.TabIndex = 0;
+            this.lblInfo.Text = "label1";
+            // 
+            // plMain
+            // 
+            this.plMain.Controls.Add(this.plContent);
+            this.plMain.Controls.Add(this.lblInfo);
+            this.plMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plMain.Location = new System.Drawing.Point(0, 0);
+            this.plMain.Margin = new System.Windows.Forms.Padding(0);
+            this.plMain.Name = "plMain";
+            this.plMain.Padding = new System.Windows.Forms.Padding(10);
+            this.plMain.Size = new System.Drawing.Size(1175, 701);
+            this.plMain.TabIndex = 3;
+            // 
+            // plContent
+            // 
+            this.plContent.BackColor = System.Drawing.SystemColors.Control;
+            this.plContent.Controls.Add(this.dgvDetail);
+            this.plContent.Controls.Add(this.textBox1);
+            this.plContent.Controls.Add(this.plButtons);
+            this.plContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plContent.Location = new System.Drawing.Point(10, 56);
+            this.plContent.Name = "plContent";
+            this.plContent.Size = new System.Drawing.Size(1155, 635);
+            this.plContent.TabIndex = 1;
+            // 
+            // dgvDetail
+            // 
+            this.dgvDetail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDetail.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDetail.Location = new System.Drawing.Point(0, 120);
+            this.dgvDetail.Name = "dgvDetail";
+            this.dgvDetail.RowTemplate.Height = 23;
+            this.dgvDetail.Size = new System.Drawing.Size(1155, 485);
+            this.dgvDetail.TabIndex = 1;
+            // 
+            // plButtons
+            // 
+            this.plButtons.Controls.Add(this.btnDelAll);
+            this.plButtons.Controls.Add(this.btnSave);
+            this.plButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.plButtons.Location = new System.Drawing.Point(0, 605);
+            this.plButtons.Name = "plButtons";
+            this.plButtons.Size = new System.Drawing.Size(1155, 30);
+            this.plButtons.TabIndex = 0;
+            // 
+            // btnDelAll
+            // 
+            this.btnDelAll.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnDelAll.Font = new System.Drawing.Font("仿宋", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnDelAll.Location = new System.Drawing.Point(975, 0);
+            this.btnDelAll.Name = "btnDelAll";
+            this.btnDelAll.Size = new System.Drawing.Size(90, 30);
+            this.btnDelAll.TabIndex = 2;
+            this.btnDelAll.Text = "删除选中";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnSave.Font = new System.Drawing.Font("仿宋", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnSave.Location = new System.Drawing.Point(1065, 0);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(90, 30);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "保存";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1155, 120);
+            this.textBox1.TabIndex = 2;
             // 
             // TechnologyQuestionEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.plMain);
             this.Name = "TechnologyQuestionEditor";
-            this.Size = new System.Drawing.Size(925, 565);
+            this.Size = new System.Drawing.Size(1175, 701);
+            this.plMain.ResumeLayout(false);
+            this.plContent.ResumeLayout(false);
+            this.plContent.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).EndInit();
+            this.plButtons.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Controls.AutoHeightLabel lblInfo;
+        private System.Windows.Forms.Panel plMain;
+        private System.Windows.Forms.Panel plContent;
+        private System.Windows.Forms.DataGridView dgvDetail;
+        private System.Windows.Forms.Panel plButtons;
+        private System.Windows.Forms.Button btnDelAll;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
