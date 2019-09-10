375b010ae43e0fcd76d1fe0ab203f45d92fda76d
wcss <wcss2020@gmail.com>
2019-09-10 11:39:36 +0800

    ...

---
+++ Code/ProjectReporterPlugin/SuperControl.cs
@@ -0,0 +1,17 @@
+﻿using PublicReporterLib.ControlAndForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PublicReporterLib
+{
+    public class SuperControl : PluginControl<ProjectReporterPlugin.PluginRoot>
+    {
+
+    }
+
+    public class SuperForm : PluginForm<ProjectReporterPlugin.PluginRoot>
+    {
+
+    }
+}
