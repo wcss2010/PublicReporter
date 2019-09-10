d4229375b93dbfd16fd493adf8d3c911bacf89f6
wcss <wcss2020@gmail.com>
2019-09-10 11:18:04 +0800

    ...

---
+++ Code/PublicReporterLib/ControlAndForms/PluginControl.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PublicReporterLib.ControlAndForms
+{
+    public partial class PluginControl<PluginRootClass> : UserControl where PluginRootClass : IReportPluginRoot
+    {
+        public PluginControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 插件入口
+        /// </summary>
+        public PluginRootClass PluginRoot
+        {
+            get
+            {
+                if (PluginLoader.CurrentPlugin is PluginRootClass)
+                {
+                    return (PluginRootClass)PluginLoader.CurrentPlugin;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ Code/PublicReporterLib/ControlAndForms/PluginForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PublicReporterLib.ControlAndForms
+{
+    public partial class PluginForm<PluginRootClass> : Form where PluginRootClass : IReportPluginRoot
+    {
+        public PluginForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 插件入口
+        /// </summary>
+        public PluginRootClass PluginRoot
+        {
+            get
+            {
+                if (PluginLoader.CurrentPlugin is PluginRootClass)
+                {
+                    return (PluginRootClass)PluginLoader.CurrentPlugin;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
