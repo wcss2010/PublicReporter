aaf921da5f0d661efcc209df2f3755944e46f415
wcss <wcss2020@gmail.com>
2019-09-29 14:47:10 +0800

    ...

---
+++ Code/PublicReporterLib/ControlAndForms/AddressControl.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace PublicReporterLib.ControlAndForms
+{
+    partial class AddressControl
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Town = new System.Windows.Forms.ComboBox();
+            this.County = new System.Windows.Forms.ComboBox();
+            this.City = new System.Windows.Forms.ComboBox();
+            this.Province = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Town
+            // 
+            this.Town.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Town.FormattingEnabled = true;
+            this.Town.Location = new System.Drawing.Point(435, 0);
+            this.Town.Name = "Town";
+            this.Town.Size = new System.Drawing.Size(121, 24);
+            this.Town.TabIndex = 7;
+            // 
+            // County
+            // 
+            this.County.Dock = System.Windows.Forms.DockStyle.Left;
+            this.County.FormattingEnabled = true;
+            this.County.Location = new System.Drawing.Point(290, 0);
+            this.County.Name = "County";
+            this.County.Size = new System.Drawing.Size(121, 24);
+            this.County.TabIndex = 6;
+            this.County.SelectedIndexChanged += new System.EventHandler(this.County_SelectedIndexChanged);
+            // 
+            // City
+            // 
+            this.City.Dock = System.Windows.Forms.DockStyle.Left;
+            this.City.FormattingEnabled = true;
+            this.City.Location = new System.Drawing.Point(145, 0);
+            this.City.Name = "City";
+            this.City.Size = new System.Drawing.Size(121, 24);
+            this.City.TabIndex = 5;
+            this.City.SelectedIndexChanged += new System.EventHandler(this.City_SelectedIndexChanged);
+            // 
+            // Province
+            // 
+            this.Province.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Province.FormattingEnabled = true;
+            this.Province.Location = new System.Drawing.Point(0, 0);
+            this.Province.Name = "Province";
+            this.Province.Size = new System.Drawing.Size(121, 24);
+            this.Province.TabIndex = 4;
+            this.Province.SelectedIndexChanged += new System.EventHandler(this.Province_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(121, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "省";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label2.Location = new System.Drawing.Point(266, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "市";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label3.Location = new System.Drawing.Point(411, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "区";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AddressControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Town);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.County);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.City);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Province);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("仿宋", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "AddressControl";
+            this.Size = new System.Drawing.Size(556, 24);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox Town;
+        private System.Windows.Forms.ComboBox County;
+        private System.Windows.Forms.ComboBox City;
+        private System.Windows.Forms.ComboBox Province;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Code/PublicReporterLib/ControlAndForms/AddressControl.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace PublicReporterLib.ControlAndForms
+{
+    public partial class AddressControl : UserControl
+    {
+        public JObject provinceList { get; private set; }
+
+        public string provincedata { get; private set; }
+
+        public ComboBox ProvinceControl { get { return Province; } }
+
+        public ComboBox CityControl { get { return City; } }
+
+        public ComboBox CountyControl { get { return County; } }
+
+        public ComboBox TownControl { get { return Town; } }
+
+        public AddressControl()
+        {
+            InitializeComponent();             
+        }
+
+        private void Province_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.City.SelectedItem = null;
+            this.City.Visible = true;
+            this.City.Items.Clear();
+
+            ComboboxItem provincebox = this.Province.SelectedItem as ComboboxItem;
+
+            string folder = @"" + provincebox.Value + ".js";
+
+            var sourceContent = PublicReporterLib.Properties.Resource.ResourceManager.GetString("_" + folder).Trim();
+
+            string[] sArray = sourceContent.Split(';');
+
+            string citylist = null;
+
+            for (int i = 0; i < sArray.Length; i++)
+            {
+                string pdata = sArray[i] as string;
+                if (!string.IsNullOrEmpty(pdata))
+                {
+                    string[] data = pdata.Split('=');
+                    var cityname = "city[" + '"' + provincebox.Value + '"' + "]";
+                    if (cityname == data[0].Trim())
+                    {
+                        citylist = data[1].Trim();
+                    }
+                    else
+                    {
+                        provincedata = sourceContent;
+                    }
+
+                }
+            }
+
+            if (!string.IsNullOrEmpty(citylist))
+            {
+                List<AreaObject> v = JsonConvert.DeserializeObject<List<AreaObject>>(citylist);
+
+                for (int i = 0; i < v.Count; i++)
+                {
+                    ComboboxItem item = new ComboboxItem();
+                    item.Value = v[i].code;
+                    item.Text = (string)v[i].name;
+                    this.City.Items.Add(item);
+                    //this.City.Text = (string)v[0].name;
+                }
+            }
+            else
+            {
+                this.City.Visible = false;
+            }
+        }
+
+        private void City_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.County.SelectedItem = null;
+            this.County.Visible = true;
+            this.County.Items.Clear();
+
+
+            ComboboxItem citybox = this.City.SelectedItem as ComboboxItem;
+
+            if (this.City.SelectedItem == null)
+                return;
+
+            string[] sArray = provincedata.Split(';');
+
+            string countylist = null;
+
+            for (int i = 0; i < sArray.Length; i++)
+            {
+                string pdata = sArray[i] as string;
+                if (!string.IsNullOrEmpty(pdata))
+                {
+                    string[] data = pdata.Split('=');
+                    var countyname = "county[" + '"' + citybox.Value + '"' + "]";
+                    if (countyname == data[0].Trim())
+                    {
+                        countylist = data[1].Trim();
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(countylist))
+            {
+                List<AreaObject> v = JsonConvert.DeserializeObject<List<AreaObject>>(countylist);
+
+                for (int i = 0; i < v.Count; i++)
+                {
+                    ComboboxItem item = new ComboboxItem();
+                    item.Value = v[i].code;
+                    item.Text = (string)v[i].name;
+                    this.County.Items.Add(item);
+                    //this.County.Text = (string)v[0].name;
+                }
+            }
+            else
+            {
+                this.County.Visible = false;
+            }
+        }
+
+        private void County_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.Town.SelectedItem = null;
+            this.Town.Visible = true;
+            this.Town.Items.Clear();
+            
+            ComboboxItem countybox = this.County.SelectedItem as ComboboxItem;
+
+            if (this.County.SelectedItem == null)
+                return;
+
+            string[] sArray = provincedata.Split(';');
+
+            string townlist = null;
+
+            for (int i = 0; i < sArray.Length; i++)
+            {
+                string pdata = sArray[i] as string;
+                if (!string.IsNullOrEmpty(pdata))
+                {
+                    string[] data = pdata.Split('=');
+                    var townname = "area[" + '"' + countybox.Value + '"' + "]";
+                    if (townname == data[0].Trim())
+                    {
+                        townlist = data[1].Trim();
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(townlist) && townlist != "[]")
+            {
+                List<AreaObject> v = JsonConvert.DeserializeObject<List<AreaObject>>(townlist);
+
+                for (int i = 0; i < v.Count; i++)
+                {
+                    ComboboxItem item = new ComboboxItem();
+                    item.Value = v[i].code;
+                    item.Text = (string)v[i].name;
+                    this.Town.Items.Add(item);
+                    //this.Town.Text = (string)v[0].name;
+                }
+            }
+            else
+            {
+                this.Town.Visible = false;
+            }
+        }
+        
+        /// <summary>
+        /// 载入省
+        /// </summary>
+        public void loadeProvince()
+        {
+            string folder = @"province.js";
+
+            var sourceContent = PublicReporterLib.Properties.Resource.ResourceManager.GetString("_" + folder).Trim();
+
+            this.provinceList = JObject.Parse(sourceContent);
+
+            int i = 0;
+            foreach (KeyValuePair<string, JToken> keyValuePair in this.provinceList)
+            {
+                ComboboxItem item = new ComboboxItem();
+                item.Value = keyValuePair.Key;
+                item.Text = (string)keyValuePair.Value;
+                this.Province.Items.Add(item);
+                if (i == 0)
+                {
+                    this.Province.Text = (string)keyValuePair.Value;
+                }
+                i++;
+            }
+        }
+
+        /// <summary>
+        /// 载入地区
+        /// </summary>
+        /// <param name="areaid"></param>
+        public void LoadAreaCode(string areaid)
+        {
+
+            string provinceid = areaid.Substring(0, 2) + "0000";
+            string cityid = areaid.Substring(0, 4) + "00";
+            string countyid = areaid.Substring(0, 6);
+            string townid = areaid;
+            
+            string folder1 = @"province.js";
+
+            var sourceContent1 = PublicReporterLib.Properties.Resource.ResourceManager.GetString("_" + folder1).Trim();
+
+            this.provinceList = JObject.Parse(sourceContent1);
+
+            foreach (KeyValuePair<string, JToken> keyValuePair in this.provinceList)
+            {
+
+                ComboboxItem item = new ComboboxItem();
+                item.Value = keyValuePair.Key;
+                item.Text = (string)keyValuePair.Value;
+                this.Province.Items.Add(item);
+                if (provinceid == keyValuePair.Key)
+                {
+                    this.Province.Text = (string)keyValuePair.Value;
+                }
+            }
+
+            string folder2 = @"" + provinceid + ".js";
+
+            var sourceContent2 = PublicReporterLib.Properties.Resource.ResourceManager.GetString("_" + folder2).Trim();
+
+            string[] sArray = sourceContent2.Split(';');
+
+            string citylist = null;
+            string countylist = null;
+            string townlist = null;
+
+            for (int i = 0; i < sArray.Length; i++)
+            {
+                string pdata = sArray[i] as string;
+                if (!string.IsNullOrEmpty(pdata))
+                {
+                    string[] data = pdata.Split('=');
+
+                    var city = provinceid;
+                    var cityname = "city[" + '"' + city + '"' + "]";
+                    if (cityname == data[0].Trim())
+                    {
+                        citylist = data[1].Trim();
+                    }
+
+                    string county = cityid;
+                    var countyname = "county[" + '"' + county + '"' + "]";
+                    if (countyname == data[0].Trim())
+                    {
+                        countylist = data[1].Trim();
+                    }
+
+                    string town = countyid;
+                    var townname = "area[" + '"' + town + '"' + "]";
+                    if (townname == data[0].Trim())
+                    {
+                        townlist = data[1].Trim();
+                    }
+
+                }
+            }
+
+            if (!string.IsNullOrEmpty(citylist))
+            {
+                List<AreaObject> v1 = JsonConvert.DeserializeObject<List<AreaObject>>(citylist);
+
+                for (int i = 0; i < v1.Count; i++)
+                {
+                    ComboboxItem item1 = new ComboboxItem();
+                    string checkcode1 = v1[i].code;
+
+                    if (cityid == checkcode1)
+                    {
+                        item1.Value = v1[i].code;
+                        item1.Text = (string)v1[i].name;
+                        this.City.Items.Add(item1);
+                        this.City.Text = (string)v1[i].name;
+                    }
+                }
+            }
+
+            if (!string.IsNullOrEmpty(countylist))
+            {
+                List<AreaObject> v2 = JsonConvert.DeserializeObject<List<AreaObject>>(countylist);
+
+                for (int j = 0; j < v2.Count; j++)
+                {
+                    ComboboxItem item2 = new ComboboxItem();
+
+                    string checkcode2 = v2[j].code;
+
+                    if (countyid == checkcode2)
+                    {
+                        item2.Value = v2[j].code;
+                        item2.Text = (string)v2[j].name;
+                        this.County.Items.Add(item2);
+                        this.County.Text = (string)v2[j].name;
+                    }
+
+                }
+            }
+
+            if (!string.IsNullOrEmpty(townlist))
+            {
+                List<AreaObject> v3 = JsonConvert.DeserializeObject<List<AreaObject>>(townlist);
+
+                for (int k = 0; k < v3.Count; k++)
+                {
+                    ComboboxItem item3 = new ComboboxItem();
+
+                    string checkcode3 = v3[k].code;
+
+                    if (townid == checkcode3)
+                    {
+                        item3.Value = v3[k].code;
+                        item3.Text = (string)v3[k].name;
+                        this.Town.Items.Add(item3);
+                        this.Town.Text = (string)v3[k].name;
+                    }
+                }
+            }
+        }
+    }
+
+    public class ComboboxItem
+    {
+        public string Text { get; set; }
+        public object Value { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+
+    public class AreaObject
+    {
+        public string code;
+        public string name;
+    }
+}
