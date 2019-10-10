cf114a22f5405a83aecef7b0c1fd0e0e46bc8eea
wcss <wcss2020@gmail.com>
2019-10-10 16:39:22 +0800

    ...

---
+++ Code/ProjectContractPlugin/DB/Entitys/DanWeiJingFeiNianDuBiao.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+using System.Text;
+
+namespace ProjectContractPlugin.DB.Entitys
+{
+    /// <summary>
+    /// 类DanWeiJingFeiNianDuBiao。
+    /// </summary>
+    [Serializable]
+    public partial class DanWeiJingFeiNianDuBiao : IEntity
+    {
+        public DanWeiJingFeiNianDuBiao() { }
+
+        public override Noear.Weed.DbTableQuery copyTo(Noear.Weed.DbTableQuery query)
+        {
+            //设置值
+            query.set("BianHao", BianHao);
+            query.set("DanWeiMing", DanWeiMing);
+            query.set("NianDu", NianDu);
+            query.set("JingFei", JingFei);
+            query.set("ZhuangTai", ZhuangTai);
+            query.set("ModifyTime", ModifyTime);
+
+            return query;
+        }
+
+        public string BianHao { get; set; }
+        public string DanWeiMing { get; set; }
+        public int NianDu { get; set; }
+        public decimal JingFei { get; set; }
+        public string ZhuangTai { get; set; }
+
+        public override void bind(Noear.Weed.GetHandlerEx source)
+        {
+            BianHao = source("BianHao").value<string>(Guid.NewGuid().ToString());
+            DanWeiMing = source("DanWeiMing").value<string>("");
+            NianDu = source("NianDu").value<int>(0);
+            JingFei = source("JingFei").value<decimal>(0);
+            ZhuangTai = source("ZhuangTai").value<string>("");
+            ModifyTime = source("ModifyTime").value<DateTime>(DateTime.Now);
+        }
+
+        public override Noear.Weed.IBinder clone()
+        {
+            return new DanWeiJingFeiNianDuBiao();
+        }
+    }
+}
---
+++ Code/ProjectContractPlugin/DB/Entitys/RenWuBiao.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+using System.Text;
+
+namespace ProjectContractPlugin.DB.Entitys
+{
+    /// <summary>
+    /// 类RenWuBiao。
+    /// </summary>
+    [Serializable]
+    public partial class RenWuBiao : IEntity
+    {
+        public RenWuBiao() { }
+
+        public override Noear.Weed.DbTableQuery copyTo(Noear.Weed.DbTableQuery query)
+        {
+            //设置值
+            query.set("BianHao", BianHao);
+            query.set("KeTiBianHao", KeTiBianHao);
+            query.set("DanWeiMing", DanWeiMing);
+            query.set("RenWuFenGong", RenWuFenGong);
+            query.set("ZhuangTai", ZhuangTai);
+            query.set("ModifyTime", ModifyTime);
+
+            return query;
+        }
+
+        public string BianHao { get; set; }
+        public string KeTiBianHao { get; set; }
+        public string DanWeiMing { get; set; }
+        public string RenWuFenGong { get; set; }
+        public string ZhuangTai { get; set; }
+
+        public override void bind(Noear.Weed.GetHandlerEx source)
+        {
+            BianHao = source("BianHao").value<string>(Guid.NewGuid().ToString());
+            KeTiBianHao = source("KeTiBianHao").value<string>("");
+            DanWeiMing = source("DanWeiMing").value<string>("");
+            RenWuFenGong = source("RenWuFenGong").value<string>("");
+            ZhuangTai = source("ZhuangTai").value<string>("");
+            ModifyTime = source("ModifyTime").value<DateTime>(DateTime.Now);
+        }
+
+        public override Noear.Weed.IBinder clone()
+        {
+            return new RenWuBiao();
+        }
+    }
+}
