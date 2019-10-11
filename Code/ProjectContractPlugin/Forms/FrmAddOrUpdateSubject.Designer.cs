e38683c0ea472546fafbbc68bab7057b458710c2
wcss <wcss2020@gmail.com>
2019-10-11 10:14:48 +0800

    ...

--- Code/ProjectContractPlugin/Forms/FrmAddOrUpdateSubject.Designer.cs
+++ Code/ProjectContractPlugin/Forms/FrmAddOrUpdateSubject.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.txtWorkDest = new System.Windows.Forms.TextBox();
             this.btnSave = new System.Windows.Forms.Button();
@@ -36,21 +37,27 @@
             this.txtSubjectName = new System.Windows.Forms.TextBox();
             this.txtWorkContent = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.txtUnitTask = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.txtWorkUnit = new System.Windows.Forms.TextBox();
             this.txtWorkOrg = new System.Windows.Forms.ComboBox();
             this.txtWorkAddress = new ProjectContractPlugin.Editor.ProjectAddressEditor();
+            this.txtWorkUnitAddress = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dgvDetail = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("仿宋", 12F);
-            this.label1.Location = new System.Drawing.Point(37, 21);
+            this.label1.Location = new System.Drawing.Point(54, 12);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(88, 16);
@@ -61,18 +68,18 @@
             // 
             this.txtWorkDest.AcceptsReturn = true;
             this.txtWorkDest.Font = new System.Drawing.Font("仿宋", 12F);
-            this.txtWorkDest.Location = new System.Drawing.Point(148, 58);
+            this.txtWorkDest.Location = new System.Drawing.Point(148, 47);
             this.txtWorkDest.Margin = new System.Windows.Forms.Padding(4);
             this.txtWorkDest.Multiline = true;
             this.txtWorkDest.Name = "txtWorkDest";
             this.txtWorkDest.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtWorkDest.Size = new System.Drawing.Size(647, 119);
+            this.txtWorkDest.Size = new System.Drawing.Size(647, 79);
             this.txtWorkDest.TabIndex = 2;
             // 
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("仿宋", 12F);
-            this.btnSave.Location = new System.Drawing.Point(550, 638);
+            this.btnSave.Location = new System.Drawing.Point(550, 649);
             this.btnSave.Margin = new System.Windows.Forms.Padding(4);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(111, 43);
@@ -85,7 +92,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("仿宋", 12F);
-            this.label2.Location = new System.Drawing.Point(38, 109);
+            this.label2.Location = new System.Drawing.Point(55, 98);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(88, 16);
@@ -95,7 +102,7 @@
             // btnClose
             // 
             this.btnClose.Font = new System.Drawing.Font("仿宋", 12F);
-            this.btnClose.Location = new System.Drawing.Point(668, 638);
+            this.btnClose.Location = new System.Drawing.Point(668, 649);
             this.btnClose.Margin = new System.Windows.Forms.Padding(4);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(111, 43);
@@ -106,7 +113,7 @@
             // 
             // txtSubjectName
             // 
-            this.txtSubjectName.Location = new System.Drawing.Point(148, 19);
+            this.txtSubjectName.Location = new System.Drawing.Point(148, 10);
             this.txtSubjectName.Margin = new System.Windows.Forms.Padding(4);
             this.txtSubjectName.Name = "txtSubjectName";
             this.txtSubjectName.Size = new System.Drawing.Size(647, 26);
@@ -116,53 +123,30 @@
             // 
             this.txtWorkContent.AcceptsReturn = true;
             this.txtWorkContent.Font = new System.Drawing.Font("仿宋", 12F);
-            this.txtWorkContent.Location = new System.Drawing.Point(148, 192);
+            this.txtWorkContent.Location = new System.Drawing.Point(148, 138);
             this.txtWorkContent.Margin = new System.Windows.Forms.Padding(4);
             this.txtWorkContent.Multiline = true;
             this.txtWorkContent.Name = "txtWorkContent";
             this.txtWorkContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtWorkContent.Size = new System.Drawing.Size(647, 141);
+            this.txtWorkContent.Size = new System.Drawing.Size(647, 89);
             this.txtWorkContent.TabIndex = 6;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("仿宋", 12F);
-            this.label3.Location = new System.Drawing.Point(38, 268);
+            this.label3.Location = new System.Drawing.Point(55, 173);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(88, 16);
             this.label3.TabIndex = 5;
             this.label3.Text = "研究内容：";
             // 
-            // txtUnitTask
-            // 
-            this.txtUnitTask.AcceptsReturn = true;
-            this.txtUnitTask.Font = new System.Drawing.Font("仿宋", 12F);
-            this.txtUnitTask.Location = new System.Drawing.Point(147, 350);
-            this.txtUnitTask.Margin = new System.Windows.Forms.Padding(4);
-            this.txtUnitTask.Multiline = true;
-            this.txtUnitTask.Name = "txtUnitTask";
-            this.txtUnitTask.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtUnitTask.Size = new System.Drawing.Size(647, 116);
-            this.txtUnitTask.TabIndex = 8;
-            // 
-            // label4
-            // 
-            this.label4.Font = new System.Drawing.Font("仿宋", 12F);
-            this.label4.Location = new System.Drawing.Point(3, 384);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(128, 59);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "参加单位分工：";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("仿宋", 12F);
-            this.label5.Location = new System.Drawing.Point(37, 489);
+            this.label5.Location = new System.Drawing.Point(55, 242);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(88, 16);
@@ -173,7 +157,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("仿宋", 12F);
-            this.label6.Location = new System.Drawing.Point(37, 537);
+            this.label6.Location = new System.Drawing.Point(55, 319);
             this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(88, 16);
@@ -184,7 +168,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("仿宋", 12F);
-            this.label7.Location = new System.Drawing.Point(37, 579);
+            this.label7.Location = new System.Drawing.Point(55, 355);
             this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(88, 16);
@@ -193,11 +177,12 @@
             // 
             // txtWorkUnit
             // 
-            this.txtWorkUnit.Location = new System.Drawing.Point(147, 486);
+            this.txtWorkUnit.Location = new System.Drawing.Point(148, 239);
             this.txtWorkUnit.Margin = new System.Windows.Forms.Padding(4);
             this.txtWorkUnit.Name = "txtWorkUnit";
             this.txtWorkUnit.Size = new System.Drawing.Size(647, 26);
             this.txtWorkUnit.TabIndex = 4;
+            this.txtWorkUnit.TextChanged += new System.EventHandler(this.txtWorkUnit_TextChanged);
             // 
             // txtWorkOrg
             // 
@@ -233,7 +218,7 @@
             "中国核工业集团公司",
             "中国工程物理研究院",
             "其它"});
-            this.txtWorkOrg.Location = new System.Drawing.Point(147, 534);
+            this.txtWorkOrg.Location = new System.Drawing.Point(148, 316);
             this.txtWorkOrg.Name = "txtWorkOrg";
             this.txtWorkOrg.Size = new System.Drawing.Size(261, 24);
             this.txtWorkOrg.TabIndex = 10;
@@ -241,26 +226,109 @@
             // txtWorkAddress
             // 
             this.txtWorkAddress.Font = new System.Drawing.Font("仿宋", 12F);
-            this.txtWorkAddress.Location = new System.Drawing.Point(147, 576);
+            this.txtWorkAddress.Location = new System.Drawing.Point(148, 352);
             this.txtWorkAddress.Margin = new System.Windows.Forms.Padding(5);
             this.txtWorkAddress.Name = "txtWorkAddress";
             this.txtWorkAddress.Size = new System.Drawing.Size(519, 27);
             this.txtWorkAddress.TabIndex = 11;
             // 
+            // txtWorkUnitAddress
+            // 
+            this.txtWorkUnitAddress.Location = new System.Drawing.Point(148, 278);
+            this.txtWorkUnitAddress.Margin = new System.Windows.Forms.Padding(4);
+            this.txtWorkUnitAddress.Name = "txtWorkUnitAddress";
+            this.txtWorkUnitAddress.Size = new System.Drawing.Size(647, 26);
+            this.txtWorkUnitAddress.TabIndex = 4;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("仿宋", 12F);
+            this.label8.Location = new System.Drawing.Point(55, 281);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 16);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "通信地址：";
+            // 
+            // dgvDetail
+            // 
+            this.dgvDetail.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCellsExceptHeader;
+            this.dgvDetail.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dgvDetail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDetail.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("仿宋", 12F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvDetail.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvDetail.Location = new System.Drawing.Point(148, 387);
+            this.dgvDetail.MultiSelect = false;
+            this.dgvDetail.Name = "dgvDetail";
+            this.dgvDetail.RowHeadersVisible = false;
+            this.dgvDetail.RowTemplate.DefaultCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvDetail.RowTemplate.Height = 23;
+            this.dgvDetail.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDetail.Size = new System.Drawing.Size(647, 246);
+            this.dgvDetail.TabIndex = 12;
+            this.dgvDetail.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDetail_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "单位名称";
+            this.Column1.MinimumWidth = 150;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column2.HeaderText = "任务分工";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "删除";
+            this.Column3.MinimumWidth = 60;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Text = "删除";
+            this.Column3.UseColumnTextForButtonValue = true;
+            this.Column3.Width = 60;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("仿宋", 12F);
+            this.label4.Location = new System.Drawing.Point(54, 495);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "单位分工：";
+            // 
             // FrmAddOrUpdateSubject
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(836, 681);
+            this.ClientSize = new System.Drawing.Size(836, 696);
+            this.Controls.Add(this.dgvDetail);
             this.Controls.Add(this.txtWorkAddress);
             this.Controls.Add(this.txtWorkOrg);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.txtUnitTask);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.txtWorkContent);
             this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtWorkUnitAddress);
             this.Controls.Add(this.txtWorkUnit);
             this.Controls.Add(this.txtSubjectName);
             this.Controls.Add(this.btnClose);
@@ -274,6 +342,7 @@
             this.MinimizeBox = false;
             this.Name = "FrmAddOrUpdateSubject";
             this.Text = "新增/编辑课题情况";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -289,13 +358,18 @@
         private System.Windows.Forms.TextBox txtSubjectName;
         private System.Windows.Forms.TextBox txtWorkContent;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtUnitTask;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox txtWorkUnit;
         private System.Windows.Forms.ComboBox txtWorkOrg;
         private Editor.ProjectAddressEditor txtWorkAddress;
+        private System.Windows.Forms.TextBox txtWorkUnitAddress;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dgvDetail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewButtonColumn Column3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Code/ProjectContractPlugin/Forms/FrmAddOrUpdateSubject.cs
+++ Code/ProjectContractPlugin/Forms/FrmAddOrUpdateSubject.cs
@@ -24,10 +24,21 @@ namespace ProjectContractPlugin.Forms
                 txtSubjectName.Text = DataObj.KeTiMingCheng;
                 txtWorkDest.Text = DataObj.KeTiYanJiuMuBiao;
                 txtWorkContent.Text = DataObj.KeTiYanJiuNeiRong;
-                txtUnitTask.Text = DataObj.KeTiCanJiaDanWeiFenGong;
+                //txtUnitTask.Text = DataObj.KeTiCanJiaDanWeiFenGong;
                 txtWorkUnit.Text = DataObj.KeTiFuZeDanWei;
+                txtWorkUnitAddress.Text = DataObj.KeTiFuZeDanWeiTongXunDiZhi;
                 txtWorkOrg.Text = DataObj.KeTiSuoShuBuMen;
                 txtWorkAddress.setAddress(DataObj.KeTiSuoShuDiDian);
+
+                List<RenWuBiao> items = ConnectionManager.Context.table("RenWuBiao").where("KeTiBianHao='" + DataObj.BianHao + "'").select("*").getList<RenWuBiao>(new RenWuBiao());
+                dgvDetail.Rows.Clear();
+                foreach (RenWuBiao rwb in items)
+                {
+                    List<object> cells = new List<object>();
+                    cells.Add(rwb.DanWeiMing);
+                    cells.Add(rwb.RenWuFenGong);
+                    dgvDetail.Rows.Add(cells.ToArray());
+                }
             }
             else
             {
@@ -47,9 +58,10 @@ namespace ProjectContractPlugin.Forms
         {
             if (String.IsNullOrEmpty(txtSubjectName.Text)
                 || String.IsNullOrEmpty(txtWorkContent.Text)
-                || String.IsNullOrEmpty(txtUnitTask.Text)
+                //|| String.IsNullOrEmpty(txtUnitTask.Text)
                 ||String.IsNullOrEmpty(txtWorkDest.Text)
                 || String.IsNullOrEmpty(txtWorkUnit.Text)
+                || String.IsNullOrEmpty(txtWorkUnitAddress.Text)
                 || String.IsNullOrEmpty(txtWorkOrg.Text)
                 || String.IsNullOrEmpty(txtWorkAddress.getAddress()))
             {
@@ -59,11 +71,12 @@ namespace ProjectContractPlugin.Forms
 
             DataObj.KeTiMingCheng = txtSubjectName.Text;
             DataObj.KeTiFuZeDanWei = txtWorkUnit.Text;
+            DataObj.KeTiFuZeDanWeiTongXunDiZhi = txtWorkUnitAddress.Text;
             DataObj.KeTiSuoShuBuMen = txtWorkOrg.Text;
             DataObj.KeTiSuoShuDiDian = txtWorkAddress.getAddress();
             DataObj.KeTiYanJiuMuBiao = txtWorkDest.Text;
             DataObj.KeTiYanJiuNeiRong = txtWorkContent.Text;
-            DataObj.KeTiCanJiaDanWeiFenGong = txtUnitTask.Text;
+            //DataObj.KeTiCanJiaDanWeiFenGong = txtUnitTask.Text;
 
             if (string.IsNullOrEmpty(DataObj.BianHao))
             {
@@ -76,7 +89,46 @@ namespace ProjectContractPlugin.Forms
             {
                 DataObj.copyTo(ConnectionManager.Context.table("KeTiBiao")).where("BianHao='" + DataObj.BianHao + "'").update();
             }
+
+            //处理单位任务表
+            ConnectionManager.Context.table("RenWuBiao").where("KeTiBianHao='" + DataObj.BianHao + "'").delete();
+            foreach (DataGridViewRow dgvRow in dgvDetail.Rows)
+            {
+                if (dgvRow.Cells[0].Value == null)
+                {
+                    continue;
+                }
+
+                RenWuBiao rwb = new RenWuBiao();
+                rwb.BianHao = Guid.NewGuid().ToString();
+                rwb.KeTiBianHao = DataObj.BianHao;
+                rwb.DanWeiMing = dgvRow.Cells[0].Value != null ? dgvRow.Cells[0].Value.ToString() : string.Empty;
+                rwb.RenWuFenGong = dgvRow.Cells[1].Value != null ? dgvRow.Cells[1].Value.ToString() : string.Empty;
+                rwb.ZhuangTai = 0;
+                rwb.ModifyTime = DateTime.Now;
+                rwb.copyTo(ConnectionManager.Context.table("RenWuBiao")).insert();
+            }
+
             DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+        private void dgvDetail_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == dgvDetail.Columns.Count - 1)
+            {
+                if (MessageBox.Show("真的要删除吗？", "提示", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    dgvDetail.Rows.RemoveAt(e.RowIndex);
+                }
+            }
+        }
+
+        private void txtWorkUnit_TextChanged(object sender, EventArgs e)
+        {
+            if (dgvDetail.Rows.Count >= 1)
+            {
+                dgvDetail.Rows[0].Cells[0].Value = txtWorkUnit.Text;
+            }
         }
     }
 }
