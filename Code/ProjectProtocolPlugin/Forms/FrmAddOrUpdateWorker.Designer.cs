96cd9638779a0176f477a9bd808c418cffc9e989
wcss <wcss2020@gmail.com>
2019-10-21 14:20:56 +0800

    ...

--- Code/ProjectProtocolPlugin/Forms/FrmAddOrUpdateWorker.Designer.cs
+++ Code/ProjectProtocolPlugin/Forms/FrmAddOrUpdateWorker.Designer.cs
@@ -265,7 +265,6 @@
             this.txtMemo.Name = "txtMemo";
             this.txtMemo.Size = new System.Drawing.Size(483, 33);
             this.txtMemo.TabIndex = 2;
-            this.txtMemo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // rbIsSubject
             // 
