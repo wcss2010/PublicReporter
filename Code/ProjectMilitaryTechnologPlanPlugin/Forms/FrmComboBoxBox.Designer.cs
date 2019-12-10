ba1fc881d9d5e6d0a2b9e662587410b3dd698358
wcss <wcss2020@gmail.com>
2019-12-10 13:48:32 +0800

    ...

---
+++ Code/ProjectMilitaryTechnologPlanPlugin/Forms/FrmComboBoxBox.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace ProjectMilitaryTechnologPlanPlugin.Forms
+{
+    partial class FrmComboBoxBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plButtons = new System.Windows.Forms.Panel();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.tvNodes = new System.Windows.Forms.TreeView();
+            this.plButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // plButtons
+            // 
+            this.plButtons.Controls.Add(this.btnOK);
+            this.plButtons.Controls.Add(this.btnCancel);
+            this.plButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.plButtons.Location = new System.Drawing.Point(0, 471);
+            this.plButtons.Margin = new System.Windows.Forms.Padding(4);
+            this.plButtons.Name = "plButtons";
+            this.plButtons.Size = new System.Drawing.Size(390, 48);
+            this.plButtons.TabIndex = 8;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnOK.Font = new System.Drawing.Font("仿宋", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnOK.Location = new System.Drawing.Point(150, 0);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(120, 48);
+            this.btnOK.TabIndex = 17;
+            this.btnOK.TabStop = false;
+            this.btnOK.Text = "确定";
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnCancel.Font = new System.Drawing.Font("仿宋", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCancel.Location = new System.Drawing.Point(270, 0);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(120, 48);
+            this.btnCancel.TabIndex = 18;
+            this.btnCancel.TabStop = false;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // tvNodes
+            // 
+            this.tvNodes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvNodes.FullRowSelect = true;
+            this.tvNodes.Location = new System.Drawing.Point(0, 0);
+            this.tvNodes.Name = "tvNodes";
+            this.tvNodes.Size = new System.Drawing.Size(390, 471);
+            this.tvNodes.TabIndex = 9;
+            // 
+            // FrmComboBoxBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 519);
+            this.Controls.Add(this.tvNodes);
+            this.Controls.Add(this.plButtons);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmComboBoxBox";
+            this.Text = "请选择";
+            this.plButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel plButtons;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TreeView tvNodes;
+    }
+}
---
+++ Code/ProjectMilitaryTechnologPlanPlugin/Forms/FrmComboBoxBox.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjectMilitaryTechnologPlanPlugin.Forms
+{
+    public partial class FrmComboBoxBox : PublicReporterLib.SuperForm
+    {
+        /// <summary>
+        /// 已选中项
+        /// </summary>
+        public TreeNode SelectedNode { get { return tvNodes.SelectedNode; } }
+
+        public FrmComboBoxBox()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 初始化下拉框列表
+        /// </summary>
+        /// <param name="nodes"></param>
+        public void initComboboxList(TreeNode[] nodes)
+        {
+            if (nodes != null)
+            {
+                tvNodes.Nodes.AddRange(nodes);
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (SelectedNode != null)
+            {
+                DialogResult = System.Windows.Forms.DialogResult.OK;
+            }
+            else
+            {
+                DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+    }
+}
