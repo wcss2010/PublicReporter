fa73505f3aae3634b6bb2d1811d518bcb4f82896
wcss <wcss2020@gmail.com>
2019-12-10 17:59:23 +0800

    ...

--- Code/ProjectMilitaryTechnologPlanPlugin/Editor/SummaryEditor.cs
+++ Code/ProjectMilitaryTechnologPlanPlugin/Editor/SummaryEditor.cs
@@ -114,25 +114,25 @@ namespace ProjectMilitaryTechnologPlanPlugin.Editor
         
         private void btnSave_Click(object sender, EventArgs e)
         {
-            //Forms.FrmWorkProcess upf = new Forms.FrmWorkProcess();
-            //upf.LabalText = "正在保存,请等待...";
-            //upf.ShowProgressWithOnlyUI();
-            //upf.PlayProgressWithOnlyUI(80);
+            Forms.FrmWorkProcess upf = new Forms.FrmWorkProcess();
+            upf.LabalText = "正在保存,请等待...";
+            upf.ShowProgressWithOnlyUI();
+            upf.PlayProgressWithOnlyUI(80);
 
-            //try
-            //{
-            //    bool result = true;
-            //    OnSaveEvent(ref result);
-            //    PublicReporterLib.PluginLoader.getLocalPluginRoot<ProjectMilitaryTechnologPlanPlugin.PluginRoot>().refreshEditors();
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show("保存失败！Ex:" + ex.ToString());
-            //}
-            //finally
-            //{
-            //    upf.CloseProgressWithOnlyUI();
-            //}
+            try
+            {
+                bool result = true;
+                OnSaveEvent(ref result);
+                PublicReporterLib.PluginLoader.getLocalPluginRoot<ProjectMilitaryTechnologPlanPlugin.PluginRoot>().refreshEditors();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("保存失败！Ex:" + ex.ToString());
+            }
+            finally
+            {
+                upf.CloseProgressWithOnlyUI();
+            }
         }
 
         public override void RefreshView()
@@ -191,7 +191,16 @@ namespace ProjectMilitaryTechnologPlanPlugin.Editor
                 ibEdit7.Text = PluginRootObj.projectObj.XiangMuLeiBie;
                 ibEdit8.Text = PluginRootObj.projectObj.ZeRenDanWei;
                 ibEdit9.Text = PluginRootObj.projectObj.XiaJiDanWei;
-                ibEdit10.Text = PluginRootObj.projectObj.BeiZhu;
+
+                if (PluginRootObj.projectObj.BeiZhu != null && PluginRootObj.projectObj.BeiZhu.Contains(UIControlConfig.rowFlag))
+                {
+                    string[] wwww = PluginRootObj.projectObj.BeiZhu.Split(new string[] { UIControlConfig.rowFlag }, StringSplitOptions.None);
+                    if (wwww != null && wwww.Length >= 2)
+                    {
+                        ibEdit10.Text = wwww[0];
+                        ibEdit10_1.Text = wwww[1];
+                    }
+                }
             }
         }
 
@@ -199,351 +208,102 @@ namespace ProjectMilitaryTechnologPlanPlugin.Editor
         {
             base.OnSaveEvent(ref result);
 
-            //if (ibEdit1.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入协议编号!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit2.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入协议名称!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit3.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入密级!");
-            //    result = false;
-            //    return;
-            //}
-            //Int32 dd4;
-            //if (Int32.TryParse(ibEdit4.Text, out dd4) == false)
-            //{
-            //    MessageBox.Show("对不起，请输入密期!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit5.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入项目负责人!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit6.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入项目负责人身份证!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit39.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入项目负责单位!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit40.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入项目负责单位所属部门!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit41.getAddress() == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入项目负责单位所属地点!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit42.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入所属领域!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit43.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入关键字!");
-            //    result = false;
-            //    return;
-            //}
-            ////if (ibEdit44.Text == string.Empty)
-            ////{
-            ////    MessageBox.Show("对不起，请输入项目负责人电话!");
-            ////    return;
-            ////}
-            ////if (ibEdit45.Text == string.Empty)
-            ////{
-            ////    MessageBox.Show("对不起，请输入项目负责单位通信地址!");
-            ////    return;
-            ////}
-            ////if (ibEdit46.Text == string.Empty)
-            ////{
-            ////    MessageBox.Show("对不起，请输入项目负责单位联系人!");
-            ////    return;
-            ////}
-            ////if (ibEdit47.Text == string.Empty)
-            ////{
-            ////    MessageBox.Show("对不起，请输入项目负责单位联系电话!");
-            ////    return;
-            ////}
-            ////DateTime dd7;
-            ////if (DateTime.TryParse(ibEdit7.Text, out dd7) == false)
-            ////{
-            ////    MessageBox.Show("对不起，请输入协议开始时间!");
-            ////    return;
-            ////}
-            ////DateTime dd8;
-            ////if (DateTime.TryParse(ibEdit8.Text, out dd8) == false)
-            ////{
-            ////    MessageBox.Show("对不起，请输入协议结束时间!");
-            ////    return;
-            ////}
-            //Single dd9;
-            //if (Single.TryParse(ibEdit9.Text, out dd9) == false)
-            //{
-            //    MessageBox.Show("对不起，请输入协议价款!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit10.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入经费管理模式!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit11.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入委托-单位名称!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit12.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入委托-单位性质!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit13.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入委托-单位法定代表人!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit14.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入委托-单位联系人!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit15.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入委托-单位联系人电话!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit16.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入委托-单位通信地址!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit17.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入委托-单位邮政编码!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit18.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入委托-单位组织机构代码!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit19.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入委托-单位税号!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit20.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入委托-单位开户名称!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit21.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入委托-单位开户银行!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit22.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入委托-单位银行帐号!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit23.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入委托-单位财务负责人!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit24.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入委托-单位财务负责人电话!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit25.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入承研-单位名称!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit26.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入承研-单位性质!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit27.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入承研-单位法定代表人!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit28.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入承研-单位联系人!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit29.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入承研-单位联系人电话!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit30.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入承研-单位通信地址!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit31.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入承研-单位邮政编码!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit32.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入承研-单位组织机构代码!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit33.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入承研-单位税号!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit34.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入承研-单位开户名称!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit35.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入承研-单位开户银行!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit36.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入承研-单位银行帐号!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit37.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入承研-单位财务负责人!");
-            //    result = false;
-            //    return;
-            //}
-            //if (ibEdit38.Text == string.Empty)
-            //{
-            //    MessageBox.Show("对不起，请输入承研-单位财务负责人电话!");
-            //    result = false;
-            //    return;
-            //}
+            if (ibEdit1.Text == string.Empty)
+            {
+                MessageBox.Show("对不起，请输入项目名称!");
+                return;
+            }
+            if (ibEdit7.SelectedItem == null)
+            {
+                MessageBox.Show("对不起，请选择项目类别!");
+                return;
+            }
+            ProjectSortObject psoo = (ProjectSortObject)ibEdit7.SelectedItem;
+            if (ibEdit2.Text == string.Empty)
+            {
+                MessageBox.Show("对不起，请输入研究目标!");
+                return;
+            }
+            if (ibEdit2.Text.Length >= psoo.InfoMin && psoo.InfoMax <= ibEdit2.Text.Length)
+            {
+                MessageBox.Show("对不起，请输入研究目标(" + psoo.InfoMin + "," + psoo.InfoMax + ")!");
+                return;
+            }
+            if (ibEdit3.Rows.Count >= psoo.TableMin && psoo.TableMax <= ibEdit3.Rows.Count)
+            {
+                MessageBox.Show("对不起，请输入研究内容(" + psoo.TableMin + "," + psoo.TableMax + ")!");
+                return;
+            }
+            if (ibEdit4.Rows.Count >= 0)
+            {
+                MessageBox.Show("对不起，请输入预期成果!");
+                return;
+            }
+            if (ibEdit5.SelectedItem == null)
+            {
+                MessageBox.Show("对不起，请输入研究周期!");
+                return;
+            }
+            Single dd6;
+            if (Single.TryParse(ibEdit6.Text, out dd6) == false)
+            {
+                MessageBox.Show("对不起，请输入经费预算!");
+                return;
+            }
+            if (ibEdit8.SelectedItem == null)
+            {
+                MessageBox.Show("对不起，请输入责任单位!");
+                return;
+            }
+            if (ibEdit9.Text == string.Empty)
+            {
+                MessageBox.Show("对不起，请输入下级单位!");
+                return;
+            }
+            if (ibEdit10.Tag == null)
+            {
+                MessageBox.Show("对不起，请输入备注!");
+                return;
+            }
 
-            //JiBenXinXiBiao obj = PublicReporterLib.PluginLoader.getLocalPluginRoot<PluginRoot>().projectObj;
-            //if (obj == null)
-            //{
-            //    PublicReporterLib.PluginLoader.getLocalPluginRoot<PluginRoot>().projectObj = new JiBenXinXiBiao();
-            //    obj = PublicReporterLib.PluginLoader.getLocalPluginRoot<PluginRoot>().projectObj;
-            //}
+            if (PluginRootObj.projectObj == null)
+            {
+                PluginRootObj.projectObj = new JiBenXinXiBiao();
+            }
 
-            //obj.HeTongBianHao = ibEdit1.Text;
-            //obj.HeTongMingCheng = ibEdit2.Text;
-            //obj.HeTongMiJi = ibEdit3.Text;
-            //obj.HeTongMiQi = (int)ibEdit4.Value;
-            //obj.HeTongFuZeRen = ibEdit5.Text;
-            //obj.HeTongFuZeRenShenFenZheng = ibEdit6.Text;
-            ////obj.HeTongFuZeRenDianHua = ibEdit44.Text;
-            //obj.HeTongFuZeDanWei = ibEdit39.Text;
-            //obj.HeTongSuoShuBuMen = ibEdit40.Text;
-            //obj.HeTongSuoShuDiDian = ibEdit41.getAddress();
-            //obj.HeTongSuoShuLingYu = ibEdit42.Text;
-            //obj.HeTongGuanJianZi = ibEdit43.Text;
-            ////obj.HeTongFuZeDanWeiTongXunDiZhi = ibEdit45.Text;
-            ////obj.HeTongFuZeDanWeiLianXiRen = ibEdit46.Text;
-            ////obj.HeTongFuZeDanWeiLianXiRenDianHua = ibEdit47.Text;
-            //obj.HeTongKaiShiShiJian = ibEdit7.Value;
-            //obj.HeTongJieShuShiJian = ibEdit8.Value;
-            //obj.HeTongJiaKuan = ibEdit9.Value;
-            //obj.HeTongJingFeiGuanLiMoShi = ibEdit10.Text;
-            //obj.WeiTuoDanWeiMingCheng = ibEdit11.Text;
-            //obj.WeiTuoDanWeiXingZhi = ibEdit12.Text;
-            //obj.WeiTuoDanWeiFaDingDaiBiaoRen = ibEdit13.Text;
-            //obj.WeiTuoDanWeiLianXiRen = ibEdit14.Text;
-            //obj.WeiTuoDanWeiLianXiRenDianHua = ibEdit15.Text;
-            //obj.WeiTuoDanWeiTongXinDiZhi = ibEdit16.Text;
-            //obj.WeiTuoDanWeiYouZhengBianMa = ibEdit17.Text;
-            //obj.WeiTuoDanWeiZuZhiJiGouDaiMa = ibEdit18.Text;
-            //obj.WeiTuoDanWeiShuiHao = ibEdit19.Text;
-            //obj.WeiTuoDanWeiKaiHuMingCheng = ibEdit20.Text;
-            //obj.WeiTuoDanWeiKaiHuYingHang = ibEdit21.Text;
-            //obj.WeiTuoDanWeiYinHangZhangHao = ibEdit22.Text;
-            //obj.WeiTuoDanWeiCaiWuFuZeRen = ibEdit23.Text;
-            //obj.WeiTuoDanWeiCaiWuFuZeRenDianHua = ibEdit24.Text;
-            //obj.ChengYanDanWeiMingCheng = ibEdit25.Text;
-            //obj.ChengYanDanWeiXingZhi = ibEdit26.Text;
-            //obj.ChengYanDanWeiFaDingDaiBiaoRen = ibEdit27.Text;
-            //obj.ChengYanDanWeiLianXiRen = ibEdit28.Text;
-            //obj.ChengYanDanWeiLianXiRenDianHua = ibEdit29.Text;
-            //obj.ChengYanDanWeiTongXinDiZhi = ibEdit30.Text;
-            //obj.ChengYanDanWeiYouZhengBianMa = ibEdit31.Text;
-            //obj.ChengYanDanWeiZuZhiJiGouDaiMa = ibEdit32.Text;
-            //obj.ChengYanDanWeiShuiHao = ibEdit33.Text;
-            //obj.ChengYanDanWeiKaiHuMingCheng = ibEdit34.Text;
-            //obj.ChengYanDanWeiKaiHuYingHang = ibEdit35.Text;
-            //obj.ChengYanDanWeiYinHangZhangHao = ibEdit36.Text;
-            //obj.ChengYanDanWeiCaiWuFuZeRen = ibEdit37.Text;
-            //obj.ChengYanDanWeiCaiWuFuZeRenDianHua = ibEdit38.Text;
+            PluginRootObj.projectObj.XiangMuMingCheng = ibEdit1.Text;
+            PluginRootObj.projectObj.YanJiuMuBiao = ibEdit2.Text;
 
-            //if (string.IsNullOrEmpty(obj.BianHao))
-            //{
-            //    obj.BianHao = Guid.NewGuid().ToString();
-            //    obj.copyTo(ConnectionManager.Context.table("JiBenXinXiBiao")).insert();
-            //}
-            //else
-            //{
-            //    obj.copyTo(ConnectionManager.Context.table("JiBenXinXiBiao")).where("BianHao='" +  obj.BianHao+ "'").update();
-            //}
+            StringBuilder sb33 = new StringBuilder();
+            StringBuilder sb44 = new StringBuilder();
+            foreach (DataGridViewRow dgvRow in ibEdit3.Rows)
+            {
+                sb33.Append(dgvRow.Cells[0].Value != null ? dgvRow.Cells[0].Value.ToString() : string.Empty).Append(UIControlConfig.rowFlag);
+            }
+            foreach (DataGridViewRow dgvRow in ibEdit4.Rows)
+            {
+                sb44.Append(dgvRow.Cells[0].Value != null ? dgvRow.Cells[0].Value.ToString() : string.Empty).Append(UIControlConfig.cellFlag).Append(dgvRow.Cells[1].Value != null ? dgvRow.Cells[1].Value.ToString() : string.Empty).Append(UIControlConfig.rowFlag);
+            }
+            PluginRootObj.projectObj.YanJiuNeiRong = sb33.ToString();
+            PluginRootObj.projectObj.YuQiChengGuo = sb44.ToString();
+
+            PluginRootObj.projectObj.YanJiuZhouQi = ((ComboBoxObject<int>)ibEdit5.SelectedItem).Tag;
+
+            PluginRootObj.projectObj.JingFeiYuSuan = decimal.Parse(ibEdit6.Text);
+            PluginRootObj.projectObj.XiangMuLeiBie = ibEdit7.Text;
+            PluginRootObj.projectObj.ZeRenDanWei = ibEdit8.Text;
+            PluginRootObj.projectObj.XiaJiDanWei = ibEdit9.Text;
+            PluginRootObj.projectObj.BeiZhu = ibEdit10.Text + UIControlConfig.rowFlag + ibEdit10_1.Text;
+
+            if (string.IsNullOrEmpty(PluginRootObj.projectObj.BianHao))
+            {
+                PluginRootObj.projectObj.BianHao = Guid.NewGuid().ToString();
+                PluginRootObj.projectObj.copyTo(ConnectionManager.Context.table("JiBenXinXiBiao")).insert();
+            }
+            else
+            {
+                PluginRootObj.projectObj.copyTo(ConnectionManager.Context.table("JiBenXinXiBiao")).where("BianHao='" + PluginRootObj.projectObj.BianHao + "'").update();
+            }
         }
 
         public override bool IsInputCompleted()
