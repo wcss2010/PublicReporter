efeb13d5674439c23216f9b3530fcdba80f342d7
wcss <wcss2020@gmail.com>
2020-01-21 15:42:20 +0800

    ...

--- Code/ProjectStrategicLeadershipPlugin/Editor/WorkerEditor.Designer.cs
+++ Code/ProjectStrategicLeadershipPlugin/Editor/WorkerEditor.Designer.cs
@@ -28,18 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.plMain = new System.Windows.Forms.Panel();
+            this.plContent = new System.Windows.Forms.Panel();
+            this.lblInfo = new AbstractEditorPlugin.Controls.AutoHeightLabel();
+            this.plButtons = new System.Windows.Forms.Panel();
+            this.plMain.SuspendLayout();
+            this.plContent.SuspendLayout();
+            this.plButtons.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 51);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1070, 599);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnSave.Font = new System.Drawing.Font("仿宋", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnSave.Location = new System.Drawing.Point(977, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(90, 29);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "保存";
+            // 
+            // plMain
+            // 
+            this.plMain.Controls.Add(this.plContent);
+            this.plMain.Controls.Add(this.plButtons);
+            this.plMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plMain.Location = new System.Drawing.Point(0, 0);
+            this.plMain.Margin = new System.Windows.Forms.Padding(0);
+            this.plMain.Name = "plMain";
+            this.plMain.Padding = new System.Windows.Forms.Padding(10);
+            this.plMain.Size = new System.Drawing.Size(1090, 705);
+            this.plMain.TabIndex = 4;
+            // 
+            // plContent
+            // 
+            this.plContent.AutoScroll = true;
+            this.plContent.BackColor = System.Drawing.SystemColors.Control;
+            this.plContent.Controls.Add(this.flowLayoutPanel1);
+            this.plContent.Controls.Add(this.lblInfo);
+            this.plContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.plContent.Location = new System.Drawing.Point(10, 10);
+            this.plContent.Name = "plContent";
+            this.plContent.Size = new System.Drawing.Size(1070, 650);
+            this.plContent.TabIndex = 1;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoHeight = true;
+            this.lblInfo.BackColor = System.Drawing.SystemColors.Control;
+            this.lblInfo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblInfo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblInfo.Font = new System.Drawing.Font("仿宋", 15.75F);
+            this.lblInfo.Location = new System.Drawing.Point(0, 0);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
+            this.lblInfo.Size = new System.Drawing.Size(1070, 51);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "请科学严谨、实事求是填写，表述要清晰准确。";
+            // 
+            // plButtons
+            // 
+            this.plButtons.Controls.Add(this.btnSave);
+            this.plButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.plButtons.Location = new System.Drawing.Point(10, 660);
+            this.plButtons.Name = "plButtons";
+            this.plButtons.Padding = new System.Windows.Forms.Padding(3);
+            this.plButtons.Size = new System.Drawing.Size(1070, 35);
+            this.plButtons.TabIndex = 0;
             // 
             // WorkerEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.plMain);
             this.Name = "WorkerEditor";
-            this.Size = new System.Drawing.Size(596, 401);
+            this.Size = new System.Drawing.Size(1090, 705);
+            this.plMain.ResumeLayout(false);
+            this.plContent.ResumeLayout(false);
+            this.plButtons.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Panel plMain;
+        private System.Windows.Forms.Panel plContent;
+        private AbstractEditorPlugin.Controls.AutoHeightLabel lblInfo;
+        private System.Windows.Forms.Panel plButtons;
     }
 }
