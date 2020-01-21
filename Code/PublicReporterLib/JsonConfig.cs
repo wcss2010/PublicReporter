844b0208c8ff2f85c650092ada9bf03b743f1b44
wcss <wcss2020@gmail.com>
2020-01-21 09:16:37 +0800

    ...

--- Code/PublicReporterLib/JsonConfig.cs
+++ Code/PublicReporterLib/JsonConfig.cs
@@ -16,6 +16,75 @@ namespace PublicReporterLib
         public static IJsonConfigInitTool JsonConfigInitTool = null;
 
         /// <summary>
+        /// 配置
+        /// </summary>
+        public static JsonConfigObject Config { get; set; }
+
+        /// <summary>
+        /// 载入配置
+        /// </summary>
+        public static void loadConfig(string configFile)
+        {
+            try
+            {
+                Config = JsonConfigObject.loadConfig(configFile);
+                if (Config == null)
+                {
+                    //加载失败，初始化配置文件
+                    initConfig(configFile);
+                }
+            }
+            catch (Exception ex)
+            {
+                //初始化
+                initConfig(configFile);
+            }
+        }
+
+        /// <summary>
+        /// 初始化配置
+        /// </summary>
+        public static void initConfig(string configFile)
+        {
+            //创建Config对象
+            Config = new JsonConfigObject();
+
+            //初始化配置
+            if (JsonConfigInitTool != null)
+            {
+                JsonConfigInitTool.initConfig(Config);
+            }
+
+            //保存初始化内容
+            saveConfig(configFile);
+
+            //重新加载一次
+            loadConfig(configFile);
+        }
+
+        /// <summary>
+        /// 保存配置
+        /// </summary>
+        public static void saveConfig(string configFile)
+        {
+            JsonConfigObject.saveConfig(configFile, Config);
+        }
+    }
+
+    /// <summary>
+    /// 配置文件初始化
+    /// </summary>
+    public interface IJsonConfigInitTool
+    {
+        void initConfig(JsonConfigObject config);
+    }
+
+    /// <summary>
+    /// 配置文件类
+    /// </summary>
+    public class JsonConfigObject
+    {
+        /// <summary>
         /// 行分割符
         /// </summary>
         public const string rowFlag = "{(<r>)}";
@@ -24,11 +93,6 @@ namespace PublicReporterLib
         /// 单元格分割符
         /// </summary>
         public const string cellFlag = "{(<c>)}";
-
-        /// <summary>
-        /// 配置
-        /// </summary>
-        public static JsonConfig Config { get; set; }
 
         private SerialDictionary<string, string> stringDict = new SerialDictionary<string, string>();
         /// <summary>
@@ -51,64 +115,38 @@ namespace PublicReporterLib
         /// <summary>
         /// 载入配置
         /// </summary>
-        public static void loadConfig(string configFile)
+        public static JsonConfigObject loadConfig(string configFile)
         {
+            JsonConfigObject configObj = null;
+
             try
             {
                 //检查是不是存在配置文件
                 if (File.Exists(configFile))
                 {
                     //读取配置文件
-                    Config = Newtonsoft.Json.JsonConvert.DeserializeObject<JsonConfig>(File.ReadAllText(configFile));
+                    configObj = Newtonsoft.Json.JsonConvert.DeserializeObject<JsonConfigObject>(File.ReadAllText(configFile));
                 }
                 else
                 {
-                    //初始化
-                    initConfig(configFile);
+                    configObj = null;
                 }
             }
             catch (Exception ex)
             {
-                //初始化
-                initConfig(configFile);
-            }
-        }
-
-        /// <summary>
-        /// 初始化配置
-        /// </summary>
-        public static void initConfig(string configFile)
-        {
-            Config = new JsonConfig();
-
-            //初始化配置
-            if (JsonConfigInitTool != null)
-            {
-                JsonConfigInitTool.initConfig(Config);
+                configObj = null;
             }
 
-            //保存初始化内容
-            saveConfig(configFile);
-
-            //重新加载一次
-            loadConfig(configFile);
+            return configObj;
         }
 
         /// <summary>
         /// 保存配置
         /// </summary>
-        public static void saveConfig(string configFile)
+        public static void saveConfig(string configFile, JsonConfigObject configObj)
         {
-            string cnt = Newtonsoft.Json.JsonConvert.SerializeObject(Config, Newtonsoft.Json.Formatting.Indented);
+            string cnt = Newtonsoft.Json.JsonConvert.SerializeObject(configObj, Newtonsoft.Json.Formatting.Indented);
             File.WriteAllText(configFile, cnt);
         }
-    }
-
-    /// <summary>
-    /// 配置文件初始化
-    /// </summary>
-    public interface IJsonConfigInitTool
-    {
-        void initConfig(JsonConfig config);
     }
 }
