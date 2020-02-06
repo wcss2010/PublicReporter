3d42db9233d6794df4f22a2535879b3d983505de
wcss <wcss2020@gmail.com>
2020-02-06 11:01:46 +0800

    ...

--- Code/ProjectStrategicLeadershipPlugin/WordPrinter.cs
+++ Code/ProjectStrategicLeadershipPlugin/WordPrinter.cs
@@ -19,7 +19,7 @@ namespace ProjectStrategicLeadershipPlugin
         /// <summary>
         /// Doc输出文件名
         /// </summary>
-        public static string outputDocFileName = "论证报告.doc";
+        public static string outputDocFileName = "战略先导计划.doc";
 
         /// <summary>
         /// 输出word内容
@@ -45,13 +45,53 @@ namespace ProjectStrategicLeadershipPlugin
             try
             {
                 AbstractEditorPlugin.AbstractPluginRoot.report(progressDialog, 20, "准备数据...", 1000);
-                
+                #region 准备数据
+                List<Subjects> subjectList = ConnectionManager.Context.table("Subjects").select("*").getList<Subjects>(new Subjects());
+                List<ProjectStep> projectstepList = ConnectionManager.Context.table("ProjectStep").select("*").getList<ProjectStep>(new ProjectStep());
+                #endregion
+
                 AbstractEditorPlugin.AbstractPluginRoot.report(progressDialog, 30, "写入基本信息...", 1000);
+                #region 写基本信息
+                writeStringToBookmark(wd, "基本信息_密级", projObj.ProjectSecretLevel);
+                writeStringToBookmark(wd, "基本信息_申报主题", projObj.ProjectTopic);
+                writeStringToBookmark(wd, "基本信息_申报方向", projObj.ProjectDirection);
+                writeStringToBookmark(wd, "基本信息_项目名称", projObj.ProjectName);
+                writeStringToBookmark(wd, "基本信息_申报单位名称", projObj.UnitName);
+                writeStringToBookmark(wd, "基本信息_申报单位常用名称", projObj.UnitRealName);
+                writeStringToBookmark(wd, "基本信息_项目负责人", projObj.ProjectMasterName);
+                writeStringToBookmark(wd, "基本信息_研究周期", projObj.TotalTime + "");
+                writeStringToBookmark(wd, "基本信息_研究经费", projObj.TotalMoney + "");
+                writeStringToBookmark(wd, "基本信息_联系人", projObj.UnitContact);
+                writeStringToBookmark(wd, "基本信息_联系电话", projObj.UnitContactPhone);
+                writeStringToBookmark(wd, "基本信息_单位所在省市", projObj.UnitAddress);
+                writeStringToBookmark(wd, "基本信息_所属大单位", projObj.UnitType2);
+                writeStringToBookmark(wd, "基本信息_申报日期", projObj.RequestTime.ToShortDateString());
+
+                writeStringToBookmark(wd, "研究内容_研究内容数量", subjectList.Count + "");
+                writeStringToBookmark(wd, "研究周期与进度安排_周期", projObj.TotalTime + "");
+                writeStringToBookmark(wd, "研究周期与进度安排_经费", projObj.TotalMoney + "");
+                writeStringToBookmark(wd, "研究周期与进度安排_阶段数", projectstepList.Count + "");
+                #endregion
 
                 AbstractEditorPlugin.AbstractPluginRoot.report(progressDialog, 40, "写入文档文件...", 1000);
-                                
-                AbstractEditorPlugin.AbstractPluginRoot.report(progressDialog, 60, "写入表格数据...", 1000);
-                
+                #region 写文档文件
+                writeFileToBookmark(wd, "项目摘要", Path.Combine(pt.filesDir, PluginRoot.tnode_1_Name) + ".txt", true);
+                writeFileToBookmark(wd, "概述_需求分析", Path.Combine(pt.filesDir, PluginRoot.tnode_2_0_Name) + ".doc", true);
+                writeFileToBookmark(wd, "概述_研究现状", Path.Combine(pt.filesDir, PluginRoot.tnode_2_1_Name) + ".doc", true);
+                writeFileToBookmark(wd, "研究目标", Path.Combine(pt.filesDir, PluginRoot.tnode_3_Name) + ".doc", true);
+                writeFileToBookmark(wd, "研究内容_之间关系", Path.Combine(pt.filesDir, PluginRoot.tnode_4_1_Name) + ".doc", true);
+                writeFileToBookmark(wd, "研究成果_研究成果及考核指标", Path.Combine(pt.filesDir, PluginRoot.tnode_5_0_Name) + ".doc", true);
+                writeFileToBookmark(wd, "研究成果_成果服务方式", Path.Combine(pt.filesDir, PluginRoot.tnode_5_1_Name) + ".doc", true);
+                writeFileToBookmark(wd, "研究基础与保障条件", Path.Combine(pt.filesDir, PluginRoot.tnode_7_Name) + ".doc", true);
+                writeFileToBookmark(wd, "项目负责人和研究团队_项目负责人", Path.Combine(pt.filesDir, PluginRoot.tnode_8_0_Name) + ".doc", true);
+                writeFileToBookmark(wd, "附件文件1", Path.Combine(pt.filesDir, PluginRoot.tnode_10_Name) + ".doc", true);
+                #endregion
+
+                AbstractEditorPlugin.AbstractPluginRoot.report(progressDialog, 60, "写入表格列表数据...", 1000);
+                #region 写表格列表数据
+
+                #endregion
+
                 AbstractEditorPlugin.AbstractPluginRoot.report(progressDialog, 90, "生成文档...", 1000);
 
                 #region 显示文档或生成文档
@@ -67,6 +107,47 @@ namespace ProjectStrategicLeadershipPlugin
             }
 
             AbstractEditorPlugin.AbstractPluginRoot.report(progressDialog, 95, "", 1000);
+        }
+
+        /// <summary>
+        /// 向固定的书签写文件
+        /// </summary>
+        /// <param name="wd"></param>
+        /// <param name="bookmarkString"></param>
+        /// <param name="docFile"></param>
+        /// <param name="isDeleteEnterFlag"></param>        
+        private static void writeFileToBookmark(WordDocument wd, string bookmarkString, string docFile, bool isDeleteEnterFlag)
+        {
+            if (docFile != null && File.Exists(docFile))
+            {
+                if (docFile.EndsWith(".txt"))
+                {
+                    //写文本文件
+                    if (wd.WordDocBuilder.MoveToBookmark(bookmarkString))
+                    {
+                        wd.writeWithNewLine(File.ReadAllText(docFile));
+                    }
+                }
+                else if (docFile.EndsWith(".doc") || docFile.EndsWith(".docx"))
+                {
+                    //写Doc文件
+                    wd.insertDocumentAfterBookMark(new Document(docFile), bookmarkString, isDeleteEnterFlag);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 向固定的书签写文字
+        /// </summary>
+        /// <param name="wd"></param>
+        /// <param name="bookmarkString"></param>
+        /// <param name="value"></param>
+        private static void writeStringToBookmark(WordDocument wd, string bookmarkString, string value)
+        {
+            if (wd.WordDocBuilder.MoveToBookmark(bookmarkString))
+            {
+                wd.WordDocBuilder.Write(value);
+            }
         }
     }
 }
