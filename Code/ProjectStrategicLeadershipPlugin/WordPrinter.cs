2ca64fb3d06f2ad5683d2a510563bb86ba08a526
wcss <wcss2020@gmail.com>
2020-02-06 13:05:45 +0800

    ...

--- Code/ProjectStrategicLeadershipPlugin/WordPrinter.cs
+++ Code/ProjectStrategicLeadershipPlugin/WordPrinter.cs
@@ -92,8 +92,14 @@ namespace ProjectStrategicLeadershipPlugin
                 AbstractEditorPlugin.AbstractPluginRoot.report(progressDialog, 60, "写入表格列表数据...", 1000);
                 #region 写表格列表数据
 
+                //查找所有表格
+                NodeCollection ncc = wd.WordDoc.GetChildNodes(NodeType.Table, true);
+
                 //课题附件头
                 string subjectFileHeadString = ProjectStrategicLeadershipPlugin.Editor.SubjectDetailEditor.SubjectFileFlag + "_";
+
+                //课题名称字典
+                Dictionary<string, string> subjectNameDict = new Dictionary<string, string>();
 
                 #region 生成----(研究内容_概述列表)
                 StringBuilder sb = new StringBuilder();
@@ -102,6 +108,9 @@ namespace ProjectStrategicLeadershipPlugin
                 {
                     subIndex++;
                     string subFlag = "内容" + GlobalTool.NumberToChinese(subIndex.ToString());
+
+                    subjectNameDict[sub.ID] = "研究内容" + GlobalTool.NumberToChinese(subIndex.ToString());
+
                     string infoString = string.Empty;
                     string infoFile = Path.Combine(pt.filesDir, subjectFileHeadString + sub.SubjectName + "_概述.txt");
                     if (File.Exists(infoFile))
@@ -165,9 +174,227 @@ namespace ProjectStrategicLeadershipPlugin
                 #endregion
 
                 #region 生成----(*经费表)
+                List<Moneys> moneyList = ConnectionManager.Context.table("Moneys").select("*").getList<Moneys>(new Moneys());
+                //取数放于字典中
+                Dictionary<string, string> tempDict = new Dictionary<string, string>();
+                foreach (Moneys ysb in moneyList)
+                {
+                    tempDict[ysb.Name] = ysb.Value;
+                }
+                foreach (Node node in ncc)
+                {
+                    Aspose.Words.Tables.Table t = (Aspose.Words.Tables.Table)node;
+                    if (t.GetText().Contains("科目名称") && t.GetText().Contains("年度申请经费"))
+                    {
+                        //金额
+                        t.Rows[2].Cells[1].RemoveAllChildren();
+                        t.Rows[2].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money1"]));
+                        ((Paragraph)t.Rows[2].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[3].Cells[1].RemoveAllChildren();
+                        t.Rows[3].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money2"]));
+                        ((Paragraph)t.Rows[3].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[4].Cells[1].RemoveAllChildren();
+                        t.Rows[4].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money3"]));
+                        ((Paragraph)t.Rows[4].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[5].Cells[1].RemoveAllChildren();
+                        t.Rows[5].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money3_1"]));
+                        ((Paragraph)t.Rows[5].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[6].Cells[1].RemoveAllChildren();
+                        t.Rows[6].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money3_2"]));
+                        ((Paragraph)t.Rows[6].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[7].Cells[1].RemoveAllChildren();
+                        t.Rows[7].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money3_3"]));
+                        ((Paragraph)t.Rows[7].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[8].Cells[1].RemoveAllChildren();
+                        t.Rows[8].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money4"]));
+                        ((Paragraph)t.Rows[8].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[9].Cells[1].RemoveAllChildren();
+                        t.Rows[9].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money5"]));
+                        ((Paragraph)t.Rows[9].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[10].Cells[1].RemoveAllChildren();
+                        t.Rows[10].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money5_1"]));
+                        ((Paragraph)t.Rows[10].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[11].Cells[1].RemoveAllChildren();
+                        t.Rows[11].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money5_2"]));
+                        ((Paragraph)t.Rows[11].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[12].Cells[1].RemoveAllChildren();
+                        t.Rows[12].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money6"]));
+                        ((Paragraph)t.Rows[12].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[13].Cells[1].RemoveAllChildren();
+                        t.Rows[13].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money7"]));
+                        ((Paragraph)t.Rows[13].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[14].Cells[1].RemoveAllChildren();
+                        t.Rows[14].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money8"]));
+                        ((Paragraph)t.Rows[14].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[15].Cells[1].RemoveAllChildren();
+                        t.Rows[15].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money9"]));
+                        ((Paragraph)t.Rows[15].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[16].Cells[1].RemoveAllChildren();
+                        t.Rows[16].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money10"]));
+                        ((Paragraph)t.Rows[16].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[17].Cells[1].RemoveAllChildren();
+                        t.Rows[17].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money11"]));
+                        ((Paragraph)t.Rows[17].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[18].Cells[1].RemoveAllChildren();
+                        t.Rows[18].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money12"]));
+                        ((Paragraph)t.Rows[18].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[19].Cells[1].RemoveAllChildren();
+                        t.Rows[19].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Money13"]));
+                        ((Paragraph)t.Rows[19].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        //备注
+                        t.Rows[2].Cells[2].RemoveAllChildren();
+                        t.Rows[2].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info1"]));
+                        t.Rows[2].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[3].Cells[2].RemoveAllChildren();
+                        t.Rows[3].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info2"]));
+                        t.Rows[3].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[4].Cells[2].RemoveAllChildren();
+                        t.Rows[4].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info3"]));
+                        t.Rows[4].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[5].Cells[2].RemoveAllChildren();
+                        t.Rows[5].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info3_1"]));
+                        t.Rows[5].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[6].Cells[2].RemoveAllChildren();
+                        t.Rows[6].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info3_2"]));
+                        t.Rows[6].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[7].Cells[2].RemoveAllChildren();
+                        t.Rows[7].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info3_3"]));
+                        t.Rows[7].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[8].Cells[2].RemoveAllChildren();
+                        t.Rows[8].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info4"]));
+                        t.Rows[8].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[9].Cells[2].RemoveAllChildren();
+                        t.Rows[9].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info5"]));
+                        t.Rows[9].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[10].Cells[2].RemoveAllChildren();
+                        t.Rows[10].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info5_1"]));
+                        t.Rows[10].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[11].Cells[2].RemoveAllChildren();
+                        t.Rows[11].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info5_2"]));
+                        t.Rows[11].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[12].Cells[2].RemoveAllChildren();
+                        t.Rows[12].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info6"]));
+                        t.Rows[12].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[13].Cells[2].RemoveAllChildren();
+                        t.Rows[13].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info7"]));
+                        t.Rows[13].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[14].Cells[2].RemoveAllChildren();
+                        t.Rows[14].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info8"]));
+                        t.Rows[14].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[15].Cells[2].RemoveAllChildren();
+                        t.Rows[15].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info9"]));
+                        t.Rows[15].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[16].Cells[2].RemoveAllChildren();
+                        t.Rows[16].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info10"]));
+                        t.Rows[16].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[17].Cells[2].RemoveAllChildren();
+                        t.Rows[17].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info11"]));
+                        t.Rows[17].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[18].Cells[2].RemoveAllChildren();
+                        t.Rows[18].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info12"]));
+                        t.Rows[18].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        t.Rows[19].Cells[2].RemoveAllChildren();
+                        t.Rows[19].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Info13"]));
+                        t.Rows[19].Cells[2].CellFormat.VerticalAlignment = Aspose.Words.Tables.CellVerticalAlignment.Center;
+
+                        //年度金额
+                        t.Rows[t.Rows.Count - 1].Cells[0].RemoveAllChildren();
+                        t.Rows[t.Rows.Count - 1].Cells[0].AppendChild(wd.newParagraph(t.Document, tempDict["Year1"]));
+                        ((Paragraph)t.Rows[t.Rows.Count - 1].Cells[0].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[t.Rows.Count - 1].Cells[1].RemoveAllChildren();
+                        t.Rows[t.Rows.Count - 1].Cells[1].AppendChild(wd.newParagraph(t.Document, tempDict["Year2"]));
+                        ((Paragraph)t.Rows[t.Rows.Count - 1].Cells[1].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+                        t.Rows[t.Rows.Count - 1].Cells[2].RemoveAllChildren();
+                        t.Rows[t.Rows.Count - 1].Cells[2].AppendChild(wd.newParagraph(t.Document, tempDict["Year3"]));
+                        ((Paragraph)t.Rows[t.Rows.Count - 1].Cells[2].ChildNodes[0]).ParagraphFormat.Alignment = ParagraphAlignment.Center;
+                        break;
+                    }
+                }
                 #endregion
 
                 #region 生成----(*人员表)
+                List<Persons> personList = ConnectionManager.Context.table("Persons").select("*").getList<Persons>(new Persons());
+                //填充数据
+                foreach (Node node in ncc)
+                {
+                    Aspose.Words.Tables.Table t = (Aspose.Words.Tables.Table)node;
+                    if (t.GetText().Contains("任务分工") && t.GetText().Contains("身份证号码") && t.GetText().Contains("项目中职务"))
+                    {
+                        //创建行
+                        for (int k = 0; k < personList.Count - 1; k++)
+                        {
+                            t.Rows.Insert(1, (Aspose.Words.Tables.Row)t.Rows[t.Rows.Count - 2].Clone(true));
+                        }
+
+                        int rowStart = 1;
+                        foreach (Persons data in personList)
+                        {
+                            wd.fillCell(true, t.Rows[rowStart].Cells[0], wd.newParagraph(wd.WordDoc, rowStart + ""));
+                            wd.fillCell(true, t.Rows[rowStart].Cells[1], wd.newParagraph(wd.WordDoc, data.Name));
+                            wd.fillCell(true, t.Rows[rowStart].Cells[2], wd.newParagraph(wd.WordDoc, data.IDCard));
+                            wd.fillCell(true, t.Rows[rowStart].Cells[3], wd.newParagraph(wd.WordDoc, data.UnitName));
+                            wd.fillCell(true, t.Rows[rowStart].Cells[4], wd.newParagraph(wd.WordDoc, data.Job));
+                            wd.fillCell(true, t.Rows[rowStart].Cells[5], wd.newParagraph(wd.WordDoc, data.Specialty));
+                            
+                            string roleNamess = "未知";
+
+                            switch (data.RoleType)
+                            {
+                                case FrmAddOrUpdateWorker.isOnlyProject:
+                                    roleNamess = "项目负责人";
+                                    break;
+                                case FrmAddOrUpdateWorker.isProjectAndSubject:
+                                    roleNamess = "项目负责人兼" + (subjectNameDict.ContainsKey(data.SubjectID) ? subjectNameDict[data.SubjectID] + data.RoleName : "未知");
+                                    break;
+                                case FrmAddOrUpdateWorker.isOnlySubject:
+                                    roleNamess = subjectNameDict.ContainsKey(data.SubjectID) ? subjectNameDict[data.SubjectID] + data.RoleName : "未知";
+                                    break;
+                            }
+                            wd.fillCell(true, t.Rows[rowStart].Cells[6], wd.newParagraph(wd.WordDoc, roleNamess));
+
+                            wd.fillCell(true, t.Rows[rowStart].Cells[7], wd.newParagraph(wd.WordDoc, data.TimeForSubject.ToString()));
+
+                            rowStart++;
+                        }
+                    }
+                }
                 #endregion
 
                 #region 生成----(*联系方式表)
