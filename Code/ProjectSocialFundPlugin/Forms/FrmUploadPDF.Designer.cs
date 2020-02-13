aab52d5015c01fa2702d223c801fc9f8eb6c0bcf
wcss <wcss2020@gmail.com>
2020-02-13 15:52:51 +0800

    ...

---
+++ Code/ProjectSocialFundPlugin/Forms/FrmUploadPDF.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace ProjectSocialFundPlugin.Forms
+{
+    partial class FrmUploadPDF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmUploadPDF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 431);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmUploadPDF";
+            this.Text = "FrmUploadPDF";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
