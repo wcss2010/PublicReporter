03e21e92f1ad40bfb2502a0304fbb97d04f15a7f
wcss <wcss2020@gmail.com>
2020-02-13 16:25:17 +0800

    ...

--- Code/ProjectSocialFundPlugin/Forms/FrmUploadPDF.Designer.cs
+++ Code/ProjectSocialFundPlugin/Forms/FrmUploadPDF.Designer.cs
@@ -28,22 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtImgFile = new System.Windows.Forms.LinkLabel();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.ofdUpload = new System.Windows.Forms.OpenFileDialog();
             this.SuspendLayout();
+            // 
+            // txtImgFile
+            // 
+            this.txtImgFile.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.txtImgFile.Location = new System.Drawing.Point(76, 20);
+            this.txtImgFile.Name = "txtImgFile";
+            this.txtImgFile.Size = new System.Drawing.Size(526, 29);
+            this.txtImgFile.TabIndex = 22;
+            this.txtImgFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txtImgFile.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.txtImgFile_LinkClicked);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(608, 20);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(50, 29);
+            this.btnSelect.TabIndex = 21;
+            this.btnSelect.Text = "选择";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("仿宋", 12F);
+            this.label11.Location = new System.Drawing.Point(13, 26);
+            this.label11.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(56, 16);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "附件：";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("仿宋", 12F);
+            this.btnClose.Location = new System.Drawing.Point(547, 66);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(111, 43);
+            this.btnClose.TabIndex = 18;
+            this.btnClose.Text = "取消";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("仿宋", 12F);
+            this.btnSave.Location = new System.Drawing.Point(429, 66);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(111, 43);
+            this.btnSave.TabIndex = 19;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // ofdUpload
+            // 
+            this.ofdUpload.Filter = "*.PDF|*.PDF";
             // 
             // FrmUploadPDF
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(523, 431);
+            this.ClientSize = new System.Drawing.Size(680, 124);
+            this.Controls.Add(this.txtImgFile);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
             this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmUploadPDF";
-            this.Text = "FrmUploadPDF";
+            this.Text = "上传PDF";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.LinkLabel txtImgFile;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.OpenFileDialog ofdUpload;
     }
 }
--- Code/ProjectSocialFundPlugin/Forms/FrmUploadPDF.cs
+++ Code/ProjectSocialFundPlugin/Forms/FrmUploadPDF.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -11,9 +13,91 @@ namespace ProjectSocialFundPlugin.Forms
 {
     public partial class FrmUploadPDF : AbstractEditorPlugin.BaseForm
     {
+        private string constPdf;
         public FrmUploadPDF()
         {
             InitializeComponent();
+
+            constPdf = Path.Combine(PluginRootObj.dataDir, "申请书.pdf");
+            if (File.Exists(constPdf))
+            {
+                txtImgFile.Text = Path.GetFileName(constPdf);
+                txtImgFile.Tag = constPdf;
+            }
+        }
+
+        private void txtImgFile_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (txtImgFile.Tag != null)
+            {
+                if (File.Exists(txtImgFile.Tag.ToString()))
+                {
+                    try
+                    {
+                        Process.Start(txtImgFile.Tag.ToString());
+                    }
+                    catch (Exception ex) { }
+                }
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtImgFile.Tag == "" || txtImgFile.Tag == null)
+            {
+                MessageBox.Show("对不起，请完善内容！", "错误");
+                return;
+            }
+            else
+            {
+                //检查是否需要传文件
+                if (txtImgFile.Tag != null)
+                {
+                    if (txtImgFile.Tag.ToString().StartsWith(PluginRootObj.dataDir))
+                    {
+                        //不需要传文件
+                    }
+                    else
+                    {
+                        if (File.Exists(txtImgFile.Tag.ToString()))
+                        {
+                            //传文件
+                            try
+                            {
+                                //上传
+                                File.Copy(txtImgFile.Tag.ToString(), constPdf);
+                            }
+                            catch (Exception ex)
+                            {
+                                //弹出错提示
+                                MessageBox.Show("对不起，文件(" + txtImgFile.Tag.ToString() + ")上传失败！Ex:" + ex.ToString());
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            //弹出错提示
+                            MessageBox.Show("对不起，文件(" + txtImgFile.Tag.ToString() + ")不存在！");
+                            return;
+                        }
+                    }
+                }
+                DialogResult = System.Windows.Forms.DialogResult.OK;
+            }
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            if (ofdUpload.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtImgFile.Text = Path.GetFileName(ofdUpload.FileName);
+                txtImgFile.Tag = ofdUpload.FileName;
+            }
         }
     }
 }
