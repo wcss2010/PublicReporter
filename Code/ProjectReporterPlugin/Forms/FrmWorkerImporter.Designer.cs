17be6e23fb635ef3876ad37478cf1aa9717d5d15
wcss <wcss2020@gmail.com>
2020-02-21 12:40:31 +0800

    ...

---
+++ Code/ProjectReporterPlugin/Forms/FrmWorkerImporter.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace ProjectReporterPlugin.Forms
+{
+    partial class FrmWorkerImporter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Column11 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.dgvDetail = new System.Windows.Forms.DataGridView();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ofdExcelDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.llTemplete = new System.Windows.Forms.LinkLabel();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Column11
+            // 
+            this.Column11.HeaderText = "项目中职务";
+            this.Column11.MinimumWidth = 170;
+            this.Column11.Name = "Column11";
+            this.Column11.ReadOnly = true;
+            this.Column11.Width = 170;
+            // 
+            // Column8
+            // 
+            this.Column8.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column8.HeaderText = "任务分工";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "每年投入时间（月）";
+            this.Column7.MinimumWidth = 180;
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 180;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "工作单位";
+            this.Column6.MinimumWidth = 100;
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "专业";
+            this.Column5.MinimumWidth = 70;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 70;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "职称";
+            this.Column4.MinimumWidth = 70;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 70;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "性别";
+            this.Column3.MinimumWidth = 70;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 70;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "姓名";
+            this.Column2.MinimumWidth = 70;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 70;
+            // 
+            // Column1
+            // 
+            this.Column1.FalseValue = "false";
+            this.Column1.HeaderText = "全选";
+            this.Column1.MinimumWidth = 60;
+            this.Column1.Name = "Column1";
+            this.Column1.TrueValue = "true";
+            this.Column1.Width = 60;
+            // 
+            // dgvDetail
+            // 
+            this.dgvDetail.AllowUserToAddRows = false;
+            this.dgvDetail.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCellsExceptHeader;
+            this.dgvDetail.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            this.dgvDetail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDetail.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9,
+            this.Column11});
+            this.dgvDetail.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDetail.Location = new System.Drawing.Point(0, 41);
+            this.dgvDetail.MultiSelect = false;
+            this.dgvDetail.Name = "dgvDetail";
+            this.dgvDetail.RowHeadersVisible = false;
+            this.dgvDetail.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("仿宋", 12F);
+            this.dgvDetail.RowTemplate.DefaultCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvDetail.RowTemplate.Height = 23;
+            this.dgvDetail.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvDetail.Size = new System.Drawing.Size(1013, 444);
+            this.dgvDetail.TabIndex = 6;
+            this.dgvDetail.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDetail_CellContentClick);
+            this.dgvDetail.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvDetail_ColumnHeaderMouseClick);
+            // 
+            // Column9
+            // 
+            this.Column9.HeaderText = "身份证号码";
+            this.Column9.MinimumWidth = 120;
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            this.Column9.Width = 120;
+            // 
+            // ofdExcelDialog
+            // 
+            this.ofdExcelDialog.Filter = "*.xls|*.xls";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(590, 8);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 30);
+            this.btnCancel.TabIndex = 0;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(367, 8);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 30);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "导入";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.btnOK);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 485);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1013, 45);
+            this.panel1.TabIndex = 5;
+            // 
+            // llTemplete
+            // 
+            this.llTemplete.Dock = System.Windows.Forms.DockStyle.Right;
+            this.llTemplete.Font = new System.Drawing.Font("仿宋", 12F);
+            this.llTemplete.Location = new System.Drawing.Point(725, 8);
+            this.llTemplete.Name = "llTemplete";
+            this.llTemplete.Size = new System.Drawing.Size(280, 25);
+            this.llTemplete.TabIndex = 9;
+            this.llTemplete.TabStop = true;
+            this.llTemplete.Text = "点击下载\"人员导入模板.xls\"";
+            this.llTemplete.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.llTemplete.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.llTemplete_LinkClicked);
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnSelect.Location = new System.Drawing.Point(602, 8);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(102, 25);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "选择Excel文件";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Dock = System.Windows.Forms.DockStyle.Left;
+            this.txtFile.Location = new System.Drawing.Point(65, 8);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(537, 26);
+            this.txtFile.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(8, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "路径：";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.llTemplete);
+            this.groupBox1.Controls.Add(this.btnSelect);
+            this.groupBox1.Controls.Add(this.txtFile);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(8);
+            this.groupBox1.Size = new System.Drawing.Size(1013, 41);
+            this.groupBox1.TabIndex = 4;
+            // 
+            // FrmWorkerImporter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1013, 530);
+            this.Controls.Add(this.dgvDetail);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmWorkerImporter";
+            this.Text = "人员导入";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDetail)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column11;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column1;
+        private System.Windows.Forms.DataGridView dgvDetail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.OpenFileDialog ofdExcelDialog;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.LinkLabel llTemplete;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel groupBox1;
+    }
+}
---
+++ Code/ProjectReporterPlugin/Forms/FrmWorkerImporter.cs
@@ -0,0 +1,331 @@
+﻿using ProjectReporterPlugin.DB;
+using ProjectReporterPlugin.DB.Entitys;
+using PublicReporterLib.Utility;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProjectReporterPlugin.Forms
+{
+    public partial class FrmWorkerImporter : PublicReporterLib.SuperForm
+    {
+        private List<Person> newPersonList = new List<Person>();
+
+        public FrmWorkerImporter()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            Persons[] personList = getSelectedPersonList();
+            if (personList.Length == 0)
+            {
+                MessageBox.Show("对不起，请选择要导入的人员！");
+                return;
+            }
+
+            foreach (Persons newObj in personList)
+            {
+                Persons oldObj = ConnectionManager.Context.table("Persons").where("IDCard='" + newObj.IDCard + "'").select("*").getItem<Persons>(new Persons());
+                if (string.IsNullOrEmpty(oldObj.ID))
+                {
+                    //insert
+                    newObj.ID = Guid.NewGuid().ToString();
+                    newObj.copyTo(ConnectionManager.Context.table("Persons")).insert();
+                }
+                else
+                {
+                    //update
+                    newObj.ID = oldObj.ID;
+                    newObj.copyTo(ConnectionManager.Context.table("Persons")).where("ID='" + newObj.ID + "'").update();
+                }
+            }
+
+            DialogResult = System.Windows.Forms.DialogResult.OK;
+        }
+
+        private Person[] getSelectedPersonList()
+        {
+            List<Persons> results = new List<Persons>();
+            foreach (DataGridViewRow dgvRow in dgvDetail.Rows)
+            {
+                DataGridViewCheckBoxCell checkboxCell = ((DataGridViewCheckBoxCell)dgvRow.Cells[0]);
+                if (checkboxCell.Value == "true")
+                {
+                    results.Add((Persons)dgvRow.Tag);
+                }
+            }
+            return results.ToArray();
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            if (ofdExcelDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtFile.Text = ofdExcelDialog.FileName;
+                loadExcel(txtFile.Text);
+            }
+        }
+
+        private void loadExcel(string xlsFile)
+        {
+            try
+            {
+                newPersonList = new List<Persons>();
+
+                //初始的课题ID
+                string defaultSubjectID = ConnectionManager.Context.table("Subjects").select("ID").getValue<string>("");
+
+                DataTable dtData = ExcelBuilder.excelToDataTable(xlsFile, "Person", true);
+                foreach (DataRow dr in dtData.Rows)
+                {
+                    string idCardStr = dr["身份证"] != null ? dr["身份证"].ToString().Trim() : string.Empty;
+                    string nameStr = dr["姓名"] != null ? dr["姓名"].ToString().Trim() : string.Empty;
+                    string sexStr = dr["性别"] != null ? dr["性别"].ToString().Trim() : string.Empty;
+                    string birthdayStr = dr["出生年月"] != null ? dr["出生年月"].ToString().Trim() : string.Empty;
+                    string jobStr = dr["职务/职称"] != null ? dr["职务/职称"].ToString().Trim() : string.Empty;
+                    string specStr = dr["所学专业"] != null ? dr["所学专业"].ToString().Trim() : string.Empty;
+                    string telephoneStr = dr["电话"] != null ? dr["电话"].ToString().Trim() : string.Empty;
+                    string mobilephoneStr = dr["手机"] != null ? dr["手机"].ToString().Trim() : string.Empty;
+                    string workunitStr = dr["工作单位"] != null ? dr["工作单位"].ToString().Trim() : string.Empty;
+                    string timeforsubjectStr = dr["每年投入时间"] != null ? dr["每年投入时间"].ToString().Trim() : string.Empty;
+                    string taskcontentStr = dr["任务分工"] != null ? dr["任务分工"].ToString().Trim() : string.Empty;
+                    string subjectStr = dr["研究内容名称(如是仅为项目负责人则为空)"] != null ? dr["研究内容名称(如是仅为项目负责人则为空)"].ToString().Trim() : string.Empty;
+                    string roletypeOnlyProjectStr = dr["仅为项目负责人"] != null ? dr["仅为项目负责人"].ToString().Trim() : string.Empty;
+                    string roletypeProjectAndSubjectStr = dr["项目负责人兼研究内容角色"] != null ? dr["项目负责人兼研究内容角色"].ToString().Trim() : string.Empty;
+                    string roletypeOnlySubjectStr = dr["仅为研究内容角色"] != null ? dr["仅为研究内容角色"].ToString().Trim() : string.Empty;
+                    string roleNameStr = dr["研究内容中职务（负责人或成员）"] != null ? dr["研究内容中职务（负责人或成员）"].ToString().Trim() : string.Empty;
+
+                    if (string.IsNullOrEmpty(roletypeOnlyProjectStr))
+                    {
+                        roletypeOnlyProjectStr = "否";
+                    }
+                    if (string.IsNullOrEmpty(roletypeProjectAndSubjectStr))
+                    {
+                        roletypeProjectAndSubjectStr = "否";
+                    }
+                    if (string.IsNullOrEmpty(roletypeOnlySubjectStr))
+                    {
+                        roletypeOnlySubjectStr = "否";
+                    }
+
+                    //检查非空
+                    foreach (DataColumn dc in dr.Table.Columns)
+                    {
+                        if (dc.ColumnName == "研究内容名称(如是仅为项目负责人则为空)" || dc.ColumnName == "仅为项目负责人" || dc.ColumnName == "项目负责人兼研究内容角色" || dc.ColumnName == "仅为研究内容角色")
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            if (dr[dc.ColumnName] == null || dr[dc.ColumnName].ToString() == string.Empty)
+                            {
+                                throw new Exception("'" + dc.ColumnName + "'不能为空！");
+                            }
+                        }
+                    }
+
+                    if (sexStr != "男" && sexStr != "女")
+                    {
+                        throw new Exception("性别只能为'男'或'女'！");
+                    }
+
+                    int intResult = 0;
+                    if (int.TryParse(timeforsubjectStr, out intResult) == false)
+                    {
+                        throw new Exception("'每年投入时间'只能是数字！");
+                    }
+
+                    if (roleNameStr != "负责人" && roleNameStr != "成员")
+                    {
+                        throw new Exception("'研究内容中职务'只能是负责人或成员！");
+                    }
+
+                    DateTime dtResult = DateTime.MinValue;
+                    if (DateTime.TryParse(birthdayStr, out dtResult) == false)
+                    {
+                        throw new Exception("'出生年月'格式有误！例如：" + DateTime.Now.ToShortDateString());
+                    }
+
+                    //判断研究内容名称是否正确
+                    if (roletypeOnlyProjectStr.Contains("否"))
+                    {
+                        int subjectCount = 0;
+                        object countObj = ConnectionManager.Context.table("Subjects").where("SubjectName='" + subjectStr + "'").select("count(*)").getValue();
+                        try
+                        {
+                            subjectCount = int.Parse(countObj.ToString());
+                        }
+                        catch (Exception ex) { }
+
+                        if (subjectCount == 0)
+                        {
+                            throw new Exception("'研究内容名称'有误！");
+                        }
+                    }
+
+                    #region 生成Persons对象
+                    Persons ppObj = new Persons();
+                    ppObj.IDCard = idCardStr;
+                    ppObj.Name = nameStr;
+                    ppObj.Sex = sexStr;
+                    ppObj.Birthday = DateTime.Parse(birthdayStr);
+                    ppObj.Job = jobStr;
+                    ppObj.Specialty = specStr;
+                    ppObj.Telephone = telephoneStr;
+                    ppObj.MobilePhone = mobilephoneStr;
+                    ppObj.UnitName = workunitStr;
+                    ppObj.TimeForSubject = int.Parse(timeforsubjectStr);
+                    ppObj.TaskContent = taskcontentStr;
+
+                    //课题ID
+                    ppObj.SubjectID = ConnectionManager.Context.table("Subjects").where("SubjectName='" + subjectStr + "'").select("ID").getValue<string>(defaultSubjectID);
+
+                    //角色类型
+                    if (roletypeOnlyProjectStr.Contains("是"))
+                    {
+                        ppObj.RoleType = FrmAddOrUpdateWorker.isOnlyProject;
+                        roleNameStr = "成员";
+                    }
+                    else if (roletypeProjectAndSubjectStr.Contains("是"))
+                    {
+                        ppObj.RoleType = FrmAddOrUpdateWorker.isProjectAndSubject;
+                    }
+                    else
+                    {
+                        ppObj.RoleType = FrmAddOrUpdateWorker.isOnlySubject;
+                    }
+
+                    //角色名称
+                    ppObj.RoleName = roleNameStr;
+                    #endregion
+
+                    newPersonList.Add(ppObj);
+                }
+
+                //查询研究内容列表
+                List<Subjects> subjectList = ConnectionManager.Context.table("Subjects").select("*").getList<Subjects>(new Subjects());
+                //生成研究内容X字典
+                int kindex = 0;
+                Dictionary<string, string> ktDict = new Dictionary<string, string>();
+                foreach (Subjects ktb in subjectList)
+                {
+                    kindex++;
+                    ktDict[ktb.ID] = "研究内容" + kindex;
+                }
+                dgvDetail.Rows.Clear();
+                foreach (Persons pObj in newPersonList)
+                {
+                    List<object> cells = new List<object>();
+                    cells.Add("true");
+                    cells.Add(pObj.Name);
+                    cells.Add(pObj.Sex);
+                    cells.Add(pObj.Job);
+                    cells.Add(pObj.Specialty);
+                    cells.Add(pObj.UnitName);
+                    cells.Add(pObj.TimeForSubject);
+                    cells.Add(pObj.TaskContent);
+                    cells.Add(pObj.IDCard);
+
+                    if (pObj.RoleType == FrmAddOrUpdateWorker.isOnlyProject)
+                    {
+                        cells.Add("项目负责人");
+                    }
+                    else
+                    {
+                        cells.Add((pObj.RoleType == FrmAddOrUpdateWorker.isProjectAndSubject ? "项目负责人兼" : "") + ((ktDict.ContainsKey(pObj.SubjectID) ? ktDict[pObj.SubjectID] : string.Empty) + pObj.RoleName));
+                    }
+
+                    int rowIndex = dgvDetail.Rows.Add(cells.ToArray());
+                    dgvDetail.Rows[rowIndex].Tag = pObj;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("对不起，导入失败！错误:" + ex.ToString());
+            }
+        }
+
+        private void dgvDetail_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvDetail.Rows.Count > e.RowIndex && e.RowIndex >= 0)
+            {
+                if (dgvDetail.Rows[e.RowIndex].Cells[0].Value == "true")
+                {
+                    dgvDetail.Rows[e.RowIndex].Cells[0].Value = "false";
+                }
+                else
+                {
+                    dgvDetail.Rows[e.RowIndex].Cells[0].Value = "true";
+                }
+                dgvDetail.EndEdit();
+            }
+        }
+
+        private void dgvDetail_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+
+        }
+
+        private void dgvDetail_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.ColumnIndex == 0)
+            {
+                dgvDetail.ClearSelection();
+                string firstValue = string.Empty;
+                foreach (DataGridViewRow dgvRow in dgvDetail.Rows)
+                {
+                    if (string.IsNullOrEmpty(firstValue))
+                    {
+                        if (dgvRow.Cells[0].Value == "true")
+                        {
+                            firstValue = "false";
+                        }
+                        else
+                        {
+                            firstValue = "true";
+                        }
+                    }
+                    ((DataGridViewCheckBoxCell)dgvRow.Cells[0]).Value = firstValue;
+                }
+                dgvDetail.EndEdit();
+            }
+        }
+
+        private void llTemplete_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string sourcePath = Path.Combine(PluginRootObj.RootDir, Path.Combine("Helper", "lianxiren.xls"));
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "*.xls|*.xls";
+            sfd.FileName = "人员导入模板.xls";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    File.Copy(sourcePath, sfd.FileName, true);
+                    Process.Start(sfd.FileName);
+
+                    MessageBox.Show("下载完成！");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("下载失败！Ex:" + ex.ToString());
+                }
+            }
+        }
+    }
+}
