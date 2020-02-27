3c14c110420bebe43bfd446242c14913d9730f3a
wcss <wcss2020@gmail.com>
2020-02-27 15:58:46 +0800

    ...

--- Code/AbstractEditorPlugin/Controls/TextBoxWithOnlyNumber.cs
+++ Code/AbstractEditorPlugin/Controls/TextBoxWithOnlyNumber.cs
@@ -11,10 +11,10 @@ namespace AbstractEditorPlugin.Controls
         {
             base.OnKeyPress(e);
 
-            if (e.KeyChar == 0x0D) e.KeyChar = (char)0;  //禁止回车键 
-            if (e.KeyChar == 0x0A) e.KeyChar = (char)0;  //禁止换行键 
-            if (e.KeyChar == 0x20) e.KeyChar = (char)0;  //禁止空格键 
+            if (e.KeyChar == 0x0D || e.KeyChar == 0x0A || e.KeyChar == 0x20) return;  //禁止回车,换行，空格键 
+
             if ((e.KeyChar == 0x2D) && (Text.Length == 0)) return;   //处理负数 
+
             if (e.KeyChar > 0x20)
             {
                 try
@@ -23,7 +23,7 @@ namespace AbstractEditorPlugin.Controls
                 }
                 catch
                 {
-                    e.KeyChar = (char)0;   //处理非法字符 
+                    return;   //处理非法字符 
                 }
             }
         }
