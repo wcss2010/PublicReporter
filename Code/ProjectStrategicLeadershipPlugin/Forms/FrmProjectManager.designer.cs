748dbbeab15405c548faf670737e82e0d8a7da1a
wcss <wcss2020@gmail.com>
2020-02-29 20:50:40 +0800

    ...

--- Code/ProjectStrategicLeadershipPlugin/Forms/FrmProjectManager.designer.cs
+++ Code/ProjectStrategicLeadershipPlugin/Forms/FrmProjectManager.designer.cs
@@ -44,9 +44,9 @@
             this.plContent.Controls.Add(this.plButtons);
             this.plContent.Dock = System.Windows.Forms.DockStyle.Fill;
             this.plContent.Location = new System.Drawing.Point(0, 0);
-            this.plContent.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.plContent.Margin = new System.Windows.Forms.Padding(4);
             this.plContent.Name = "plContent";
-            this.plContent.Size = new System.Drawing.Size(913, 768);
+            this.plContent.Size = new System.Drawing.Size(736, 205);
             this.plContent.TabIndex = 9;
             // 
             // tvProject
@@ -56,9 +56,9 @@
             this.tvProject.FullRowSelect = true;
             this.tvProject.HideSelection = false;
             this.tvProject.Location = new System.Drawing.Point(0, 0);
-            this.tvProject.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tvProject.Margin = new System.Windows.Forms.Padding(4);
             this.tvProject.Name = "tvProject";
-            this.tvProject.Size = new System.Drawing.Size(913, 720);
+            this.tvProject.Size = new System.Drawing.Size(736, 157);
             this.tvProject.TabIndex = 8;
             // 
             // plButtons
@@ -66,18 +66,18 @@
             this.plButtons.Controls.Add(this.btnDel);
             this.plButtons.Controls.Add(this.btnOpen);
             this.plButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.plButtons.Location = new System.Drawing.Point(0, 720);
-            this.plButtons.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.plButtons.Location = new System.Drawing.Point(0, 157);
+            this.plButtons.Margin = new System.Windows.Forms.Padding(4);
             this.plButtons.Name = "plButtons";
-            this.plButtons.Size = new System.Drawing.Size(913, 48);
+            this.plButtons.Size = new System.Drawing.Size(736, 48);
             this.plButtons.TabIndex = 7;
             // 
             // btnDel
             // 
             this.btnDel.Dock = System.Windows.Forms.DockStyle.Right;
             this.btnDel.Font = new System.Drawing.Font("仿宋", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.btnDel.Location = new System.Drawing.Point(713, 0);
-            this.btnDel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDel.Location = new System.Drawing.Point(536, 0);
+            this.btnDel.Margin = new System.Windows.Forms.Padding(4);
             this.btnDel.Name = "btnDel";
             this.btnDel.Size = new System.Drawing.Size(100, 48);
             this.btnDel.TabIndex = 31;
@@ -88,8 +88,8 @@
             // 
             this.btnOpen.Dock = System.Windows.Forms.DockStyle.Right;
             this.btnOpen.Font = new System.Drawing.Font("仿宋", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.btnOpen.Location = new System.Drawing.Point(813, 0);
-            this.btnOpen.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOpen.Location = new System.Drawing.Point(636, 0);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(4);
             this.btnOpen.Name = "btnOpen";
             this.btnOpen.Size = new System.Drawing.Size(100, 48);
             this.btnOpen.TabIndex = 29;
@@ -104,9 +104,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(913, 768);
+            this.ClientSize = new System.Drawing.Size(736, 205);
             this.Controls.Add(this.plContent);
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmProjectManager";
