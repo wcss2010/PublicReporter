4abe6ee2bea47040d24e85a5ae9d76c59b15e94a
wcss <wcss2020@gmail.com>
2020-03-01 17:50:08 +0800

    ...

--- Code/ProjectStrategicLeadershipPlugin/Forms/FrmPkgExport.cs
+++ Code/ProjectStrategicLeadershipPlugin/Forms/FrmPkgExport.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Windows.Forms;
 
@@ -15,6 +17,7 @@ namespace ProjectStrategicLeadershipPlugin.Forms
             InitializeComponent();
 
             llMainDocument.Text = "使用word打开'" + WordPrinter.outputDocFileName + "'";
+            txtReadme.LoadFile(Path.Combine(PluginRootObj.RootDir, Path.Combine("Helper", "exportReadme.rtf")));
         }
 
         private void btnOK_Click(object sender, EventArgs e)
@@ -29,7 +32,15 @@ namespace ProjectStrategicLeadershipPlugin.Forms
 
         private void llMainDocument_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-
+            string docFile = Path.Combine(PluginRootObj.dataDir, WordPrinter.outputDocFileName);
+            if (File.Exists(docFile))
+            {
+                try
+                {
+                    Process.Start(docFile);
+                }
+                catch (Exception ex) { }
+            }
         }
     }
 }
