7178b30d702517402f4952e0720d5894c641efe5
wcss <wcss2020@gmail.com>
2020-04-10 19:11:30 +0800

    ...

---
+++ Code/AbstractEditorPlugin/Forms/FrmUploadUserPDF.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace AbstractEditorPlugin.Forms
+{
+    partial class FrmUploadUserPDF
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plButtons = new System.Windows.Forms.Panel();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.lklPDFFile = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.ofdPDFS = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.plButtons.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // plButtons
+            // 
+            this.plButtons.Controls.Add(this.btnOK);
+            this.plButtons.Controls.Add(this.btnCancel);
+            this.plButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.plButtons.Location = new System.Drawing.Point(0, 115);
+            this.plButtons.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.plButtons.Name = "plButtons";
+            this.plButtons.Size = new System.Drawing.Size(594, 40);
+            this.plButtons.TabIndex = 14;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnOK.Font = new System.Drawing.Font("仿宋", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnOK.Location = new System.Drawing.Point(428, 0);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(83, 40);
+            this.btnOK.TabIndex = 17;
+            this.btnOK.TabStop = false;
+            this.btnOK.Text = "上传";
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnCancel.Font = new System.Drawing.Font("仿宋", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnCancel.Location = new System.Drawing.Point(511, 0);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(83, 40);
+            this.btnCancel.TabIndex = 18;
+            this.btnCancel.TabStop = false;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // lklPDFFile
+            // 
+            this.lklPDFFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lklPDFFile.Location = new System.Drawing.Point(90, 10);
+            this.lklPDFFile.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lklPDFFile.Name = "lklPDFFile";
+            this.lklPDFFile.Size = new System.Drawing.Size(422, 34);
+            this.lklPDFFile.TabIndex = 11;
+            this.lklPDFFile.TabStop = true;
+            this.lklPDFFile.Text = "......";
+            this.lklPDFFile.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lklPDFFile_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 34);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "路径：";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnSelect.Location = new System.Drawing.Point(512, 10);
+            this.btnSelect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(72, 34);
+            this.btnSelect.TabIndex = 12;
+            this.btnSelect.Text = "选择";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // ofdPDFS
+            // 
+            this.ofdPDFS.Filter = "*.pdf|*.pdf";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lklPDFFile);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnSelect);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 61);
+            this.panel1.Margin = new System.Windows.Forms.Padding(0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(10);
+            this.panel1.Size = new System.Drawing.Size(594, 54);
+            this.panel1.TabIndex = 15;
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblWelcome.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblWelcome.Font = new System.Drawing.Font("仿宋", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lblWelcome.Location = new System.Drawing.Point(0, 0);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(594, 61);
+            this.lblWelcome.TabIndex = 16;
+            this.lblWelcome.Text = "XXXXXXXXXXX";
+            this.lblWelcome.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmUploadPDF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 155);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.plButtons);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmUploadPDF";
+            this.Text = "上传PDF";
+            this.plButtons.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel plButtons;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.LinkLabel lklPDFFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.OpenFileDialog ofdPDFS;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblWelcome;
+    }
+}
---
+++ Code/AbstractEditorPlugin/Forms/FrmUploadUserPDF.cs
@@ -0,0 +1,110 @@
+﻿using PublicReporterLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AbstractEditorPlugin.Forms
+{
+    public partial class FrmUploadUserPDF : BaseForm
+    {
+        /// <summary>
+        /// PDF目标文件名
+        /// </summary>
+        public string PDFDestName { get; set; }
+
+        public FrmUploadUserPDF(string welcomeText, string pdfName)
+        {
+            InitializeComponent();
+
+            lblWelcome.Text = welcomeText;
+            PDFDestName = pdfName;
+
+            if (string.IsNullOrEmpty(PDFDestName))
+            {
+                PDFDestName = "用户上传的PDF";
+            }
+
+            lklPDFFile.Text = Path.GetFileName(getPDFFile());
+            if (string.IsNullOrEmpty(lklPDFFile.Text))
+            {
+                lklPDFFile.Text = "......";
+            }
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //删除原来的PDF文件
+                string source = getPDFFile();
+                if (File.Exists(source))
+                {
+                    File.Delete(source);
+                }
+
+                //复制PDF到目标位置
+                File.Copy(ofdPDFS.FileName, Path.Combine(PluginRootObj.dataDir, PDFDestName + ".pdf"), true);
+
+                DialogResult = System.Windows.Forms.DialogResult.OK;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("对不起，上传失败！Ex:" + ex.ToString());
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = System.Windows.Forms.DialogResult.Cancel;
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            if (ofdPDFS.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                lklPDFFile.Text = ofdPDFS.FileName;
+            }
+        }
+
+        private void lklPDFFile_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string destPDF = Path.Combine(PluginRootObj.dataDir, lklPDFFile.Text);
+
+            if (File.Exists(destPDF))
+            {
+                try
+                {
+                    Process.Start(destPDF);
+                }
+                catch (Exception ex) { }
+            }
+        }
+
+        /// <summary>
+        /// 本地PDF文件
+        /// </summary>
+        /// <returns></returns>
+        public static string getPDFFile()
+        {
+            string result = string.Empty;
+            string[] files = Directory.GetFiles(((AbstractPluginRoot)PluginLoader.CurrentPlugin).dataDir);
+            foreach (string s in files)
+            {
+                if (s != null && s.EndsWith(".pdf"))
+                {
+                    result = s;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
