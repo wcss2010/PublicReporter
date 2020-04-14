c6f278c9d9b95f066eaa95f0b9ca859766ec3339
wcss <wcss2020@gmail.com>
2020-04-14 11:38:36 +0800

    ...

---
+++ Code/AbstractEditorPlugin/Utility/DistrictCodeTable.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractEditorPlugin.Utility
+{
+    public class DistrictCodeTable
+    {
+        /// <summary>
+        /// 地区代码表(默认为空，需初始化：简称、全称)
+        /// </summary>
+        public Dictionary<string, string> m_DistrictTB = new Dictionary<string, string>();
+
+        /// <summary>
+        /// 初始化：地区代码：简称
+        /// </summary>
+        public void InitDistrictTable_Short()
+        {
+            m_DistrictTB.Clear();
+            //11-15 京、津、冀、晋、蒙
+            m_DistrictTB.Add("11", "京");
+            m_DistrictTB.Add("12", "津");
+            m_DistrictTB.Add("13", "冀");
+            m_DistrictTB.Add("14", "晋");
+            m_DistrictTB.Add("15", "蒙");
+            //21-23 辽、吉、黑
+            m_DistrictTB.Add("21", "辽");
+            m_DistrictTB.Add("22", "吉");
+            m_DistrictTB.Add("23", "黑");
+            //31-37 沪、苏、浙、皖、闽、赣、鲁
+            m_DistrictTB.Add("31", "沪");
+            m_DistrictTB.Add("32", "苏");
+            m_DistrictTB.Add("33", "浙");
+            m_DistrictTB.Add("34", "皖");
+            m_DistrictTB.Add("35", "闽");
+            m_DistrictTB.Add("36", "赣");
+            m_DistrictTB.Add("37", "鲁");
+            //41-46 豫、鄂、湘、粤、桂、琼
+            m_DistrictTB.Add("41", "豫");
+            m_DistrictTB.Add("42", "鄂");
+            m_DistrictTB.Add("43", "湘");
+            m_DistrictTB.Add("44", "粤");
+            m_DistrictTB.Add("45", "桂");
+            m_DistrictTB.Add("46", "琼");
+            //50-54 渝、川、贵、云、藏
+            m_DistrictTB.Add("50", "渝");
+            m_DistrictTB.Add("51", "川");
+            m_DistrictTB.Add("52", "贵");
+            m_DistrictTB.Add("53", "云");
+            m_DistrictTB.Add("54", "藏");
+            //61-65 陕、甘、青、宁、新
+            m_DistrictTB.Add("61", "陕");
+            m_DistrictTB.Add("62", "甘");
+            m_DistrictTB.Add("63", "青");
+            m_DistrictTB.Add("64", "宁");
+            m_DistrictTB.Add("65", "新");
+            //71 台湾
+            m_DistrictTB.Add("71", "台");
+            //81-82 港、澳
+            m_DistrictTB.Add("81", "港");
+            m_DistrictTB.Add("82", "澳");
+            //91 国外
+            m_DistrictTB.Add("91", "外");
+        }
+
+        /// <summary>
+        /// 初始化：地区代码：全称
+        /// </summary>
+        public void InitDistrictTable_Full()
+        {
+            m_DistrictTB.Clear();
+            //11-15 京、津、冀、晋、蒙
+            m_DistrictTB.Add("11", "北京");
+            m_DistrictTB.Add("12", "天津");
+            m_DistrictTB.Add("13", "河北");
+            m_DistrictTB.Add("14", "山西");
+            m_DistrictTB.Add("15", "内蒙古");
+            //21-23 辽、吉、黑
+            m_DistrictTB.Add("21", "辽宁");
+            m_DistrictTB.Add("22", "吉林");
+            m_DistrictTB.Add("23", "黑龙江");
+            //31-37 沪、苏、浙、皖、闽、赣、鲁
+            m_DistrictTB.Add("31", "上海");
+            m_DistrictTB.Add("32", "江苏");
+            m_DistrictTB.Add("33", "浙江");
+            m_DistrictTB.Add("34", "安徽");
+            m_DistrictTB.Add("35", "福建");
+            m_DistrictTB.Add("36", "江西");
+            m_DistrictTB.Add("37", "山东");
+            //41-46 豫、鄂、湘、粤、桂、琼
+            m_DistrictTB.Add("41", "河南");
+            m_DistrictTB.Add("42", "湖北");
+            m_DistrictTB.Add("43", "湖南");
+            m_DistrictTB.Add("44", "广东");
+            m_DistrictTB.Add("45", "广西");
+            m_DistrictTB.Add("46", "海南");
+            //50-54 渝、川、贵、云、藏
+            m_DistrictTB.Add("50", "重庆");
+            m_DistrictTB.Add("51", "四川");
+            m_DistrictTB.Add("52", "贵州");
+            m_DistrictTB.Add("53", "云南");
+            m_DistrictTB.Add("54", "西藏");
+            //61-65 陕、甘、青、宁、新
+            m_DistrictTB.Add("61", "陕西");
+            m_DistrictTB.Add("62", "甘肃");
+            m_DistrictTB.Add("63", "青海");
+            m_DistrictTB.Add("64", "宁夏");
+            m_DistrictTB.Add("65", "新疆");
+            //71 台湾
+            m_DistrictTB.Add("71", "台湾");
+            //81-82 港、澳
+            m_DistrictTB.Add("81", "香港");
+            m_DistrictTB.Add("82", "澳门");
+            //91 国外
+            m_DistrictTB.Add("91", "国外");
+        }
+
+        /// <summary>
+        /// 地区代码返回结果类型：Full(全称)、Short(简称)
+        /// </summary>
+        public enum DistrictResultType
+        {
+            /// <summary>
+            /// 全称
+            /// </summary>
+            Full,
+            /// <summary>
+            /// 简称
+            /// </summary>
+            Short
+        }
+
+        /// <summary>
+        /// 通过两位地区码得到对应的地区名称
+        /// </summary>
+        /// <param name="code">两位地区码</param>
+        /// <param name="resType">返回类型：Full(全称)、Short(简称)</param>
+        /// <returns>对应的地区名称</returns>
+        public string GetDistrictCode(string code, int resType)
+        {
+            try
+            {
+                string codeStr = "错误地区";
+                if (code.Length == 2)
+                {
+                    //初始化：全称
+                    if (resType == (int)DistrictResultType.Full)
+                    {
+                        InitDistrictTable_Full();
+                    }
+                    //初始化：简称
+                    if (resType == (int)DistrictResultType.Short)
+                    {
+                        InitDistrictTable_Short();
+                    }
+                    //获取对应键值的结果
+                    if (m_DistrictTB.ContainsKey(code))
+                    {
+                        codeStr = m_DistrictTB[code].ToString();
+                    }
+                }
+                return codeStr;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Code/AbstractEditorPlugin/Utility/GetIDCardInfoCls.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AbstractEditorPlugin.Utility
+{
+    public class GetIDCardInfoCls
+    {
+        //地区代码表
+        private DistrictCodeTable m_DistrictCode = new DistrictCodeTable();
+
+        /// <summary>
+        /// 分析身份证号，返回：生日、性别、地区(简)、地区(全)
+        /// </summary>
+        public string[] AnalyzeIDCard(string strIDNum, out bool isOK)
+        {
+            try
+            {
+                string[] retInfo = new string[4];
+                int strLen = strIDNum.Length;//数据长度
+
+                #region 粗过滤：用正则表达式去校验身份证号的输入
+                string rule = string.Empty;
+                if (strLen == 15) rule = @"\d{6}\d{2}[0-1]\d[0-3]\d{4}";
+                if (strLen == 18) rule = @"\d{6}[1-2]\d{3}[0-1]\d[0-3]\d{4}[0-9X]";
+                if (!rule.Equals(string.Empty))
+                {
+                    System.Text.RegularExpressions.Regex regex = new Regex(rule);
+                    isOK = regex.IsMatch(strIDNum);
+                }
+                else
+                {
+                    isOK = false;
+                }
+                #endregion
+
+                #region 细过滤：判断前两位数字是否为正确的地区代码
+                if (isOK)
+                {
+                    string headAB = strIDNum.Substring(0, 2);
+                    m_DistrictCode.InitDistrictTable_Short();
+                    //若前两位数字在地区代码中不存在，则为假
+                    if (!m_DistrictCode.m_DistrictTB.ContainsKey(headAB))
+                    {
+                        isOK = false;
+                    }
+                }
+                #endregion
+
+                #region 细过滤：出生日期是否可转换
+                if (isOK)
+                {
+                    //更改15位身份证中的日期格式
+                    if (strLen == 15) strIDNum = strIDNum.Insert(6, "19");
+                    string testBir = strIDNum.Substring(6, 8).Insert(4, "-").Insert(7, "-");
+                    DateTime testRes;
+                    DateTime.TryParse(testBir, out testRes);
+                    //若转换结果为最小值，这说明转换失败
+                    if (testRes == DateTime.MinValue)
+                    {
+                        isOK = false;
+                    }
+                }
+                #endregion
+
+                //基础检查完成后的操作
+                if (!isOK)
+                {
+                    throw new Exception("请输入15位或18位有效身份证号！");
+                }
+                else
+                {
+                    retInfo = GetIDCardInfo(strIDNum);
+                }
+                return retInfo;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 获取身份证信息
+        /// </summary>
+        /// <returns>生日、性别、地区(简)、地区(全)</returns>
+        private string[] GetIDCardInfo(string IDCardNo)
+        {
+            try
+            {
+                string[] info = new string[4];
+                int sex = -1;
+                //更改15位身份证中的日期格式
+                if (IDCardNo.Length == 15) IDCardNo = IDCardNo.Insert(6, "19");
+                //info[0]==生日
+                info[0] = IDCardNo.Substring(6, 8).Insert(4, "-").Insert(7, "-");
+                //info[1]==性别
+                sex = int.Parse(IDCardNo.Substring(16, 1));
+                info[1] = sex % 2 == 1 ? "男" : "女";
+                //info[2]==地区(简)
+                info[2] = m_DistrictCode.GetDistrictCode(IDCardNo.Substring(0, 2), (int)DistrictCodeTable.DistrictResultType.Short);
+                //info[3]==地区(全)
+                info[3] = m_DistrictCode.GetDistrictCode(IDCardNo.Substring(0, 2), (int)DistrictCodeTable.DistrictResultType.Full);
+                return info;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
