ae0343ceb117ab59f7ce4f1595b1fd4fa7719a6f
wcss <wcss2020@gmail.com>
2020-04-15 14:34:44 +0800

    ....

--- Code/ProjectMilitaryTechnologPlanPlugin/PluginRoot.cs
+++ Code/ProjectMilitaryTechnologPlanPlugin/PluginRoot.cs
@@ -257,7 +257,7 @@ namespace ProjectMilitaryTechnologPlanPlugin
             else
             {
                 //复制DataTemplete中的Current到Data中
-                FileOp.CopyDirectory(Path.Combine(RootDir, Path.Combine("DataTemplete", "Current")), Path.Combine(RootDir, Path.Combine("Data", "Current")), true);
+                FileOp.copyDirectory(Path.Combine(RootDir, Path.Combine("DataTemplete", "Current")), Path.Combine(RootDir, Path.Combine("Data", "Current")), true);
 
                 //打开数据库连接
                 ConnectionManager.Open(dbFile);
@@ -957,6 +957,43 @@ namespace ProjectMilitaryTechnologPlanPlugin
                 }
             }
             return result;
+        }
+    }
+
+    public class FileOp
+    {
+        public static bool copyDirectory(string SourcePath, string DestinationPath, bool overwriteexisting)
+        {
+            bool ret = false;
+            try
+            {
+                SourcePath = SourcePath.EndsWith(@"\") ? SourcePath : SourcePath + @"\";
+                DestinationPath = DestinationPath.EndsWith(@"\") ? DestinationPath : DestinationPath + @"\";
+
+                if (Directory.Exists(SourcePath))
+                {
+                    if (Directory.Exists(DestinationPath) == false)
+                        Directory.CreateDirectory(DestinationPath);
+
+                    foreach (string fls in Directory.GetFiles(SourcePath))
+                    {
+                        FileInfo flinfo = new FileInfo(fls);
+                        flinfo.CopyTo(DestinationPath + flinfo.Name, overwriteexisting);
+                    }
+                    foreach (string drs in Directory.GetDirectories(SourcePath))
+                    {
+                        DirectoryInfo drinfo = new DirectoryInfo(drs);
+                        if (copyDirectory(drs, DestinationPath + drinfo.Name, overwriteexisting) == false)
+                            ret = false;
+                    }
+                }
+                ret = true;
+            }
+            catch (Exception ex)
+            {
+                ret = false;
+            }
+            return ret;
         }
     }
 }
